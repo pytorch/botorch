b1d1c23f15cd998ddd128dc816f23259bd577777
Max Balandat <balandat@fb.com>
2018-10-17 18:32:42 -0700

    Efficient batched L-BFGS updates
    Summary:
    Implements an efficient batched version of updating the compact representations of the L-BFGS low-rank approximations of the Hessian and its inverse. To be used in the QP solver (the inner solver of the SQP algorithm).
    
    For a description of the updates, see Section 2.5 of the following pdf: {F141532639}
    
    The description in the pdf is for the non-batched case, the implementation here does these updates in a batched fashion.
    
    Note that this will throw a ValueError when computing `gamma` turns out to mean dividing by zero. This happens when  the underlying function is locally flat (e.g. if the function is linear then all the `y_k` will be zero)
    
    Reviewed By: bletham
    
    Differential Revision: D10361906
    
    fbshipit-source-id: 67ef5a061fa273b8a5150733d9c417ced2a7eb54

--- botorch/optim/__init__.py
+++ botorch/optim/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from .batch_lbfgs import batch_compact_lbfgs_updates
 from .constraints import soft_eval_constraint
 from .converter import numpy_to_state_dict, state_dict_to_numpy
 from .initializers import q_batch_initialization
@@ -7,6 +8,7 @@ from .random_restart import random_restarts
 
 
 __all__ = [
+    batch_compact_lbfgs_updates,
     numpy_to_state_dict,
     q_batch_initialization,
     random_restarts,
