d5934d8f4e1f49cf7dd9af2322e59fbd942d27f4
Sam Daulton <samdaulton@instagram.com>
2018-11-13 21:26:31 -0800

    Move SingleTaskGP from lazarus to botorch
    Summary: This model will be nice for botorch examples.
    
    Reviewed By: Balandat
    
    Differential Revision: D13044154
    
    fbshipit-source-id: b5c653484f29b73fd0807b420971c104c1b6097a

--- botorch/models/gp_regression.py
+++ botorch/models/gp_regression.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
 from gpytorch.distributions import MultivariateNormal
-from gpytorch.kernels import RBFKernel, ScaleKernel
+from gpytorch.kernels import MaternKernel, RBFKernel, ScaleKernel
 from gpytorch.likelihoods import Likelihood
 from gpytorch.means import ConstantMean
 from gpytorch.models import ExactGP
+from gpytorch.priors import GammaPrior
 from torch import Tensor
 
 
@@ -25,6 +26,33 @@ class GPRegressionModel(ExactGP):
         self.covar_module = ScaleKernel(
             RBFKernel(ard_num_dims=ard_num_dims, batch_size=batch_size),
             batch_size=batch_size,
+        )
+
+    def forward(self, x: Tensor) -> MultivariateNormal:
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return MultivariateNormal(mean_x, covar_x)
+
+
+class SingleTaskGP(ExactGP):
+    """
+    Class implementing a single task exact GP using relatively strong priors on
+    the Kernel hyperparameters, which work best when covariates are normalized
+    to the unit cube and outcomes are standardized (zero mean, unit variance).
+    """
+
+    def __init__(
+        self, train_X: Tensor, train_Y: Tensor, likelihood: Likelihood
+    ) -> None:
+        super().__init__(train_X, train_Y, likelihood)
+        self.mean_module = ConstantMean()
+        self.covar_module = ScaleKernel(
+            MaternKernel(
+                nu=2.5,
+                ard_num_dims=train_X.shape[1] if train_X.ndimension() > 1 else None,
+                log_lengthscale_prior=GammaPrior(2.0, 5.0, log_transform=True),
+            ),
+            log_outputscale_prior=GammaPrior(1.1, 0.05, log_transform=True),
         )
 
     def forward(self, x: Tensor) -> MultivariateNormal:
