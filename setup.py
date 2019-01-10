4ded3b5c30f91a61f668401a3aaad7a92814cefd
Max Balandat <balandat@fb.com>
2019-01-09 18:28:55 -0800

    Add setup.py
    Summary:
    Simplify installation (and building of cython).
    
    For now this requires both `numpy` and `cython` to be available at setup time. We could also ship generated `.c` files in the furture (see e.g.
    https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules), but that is low-pri given that we want to move to the torch native Sobol engine from https://github.com/pytorch/pytorch/pull/10505 (if that finally makes its way in...)
    
    Reviewed By: bkarrer
    
    Differential Revision: D13592395
    
    fbshipit-source-id: 4bd2c7fada88dfad2bffce7c8b31dae3fca47e1f

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy
+from Cython.Build import cythonize
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+
+
+extensions = [Extension("botorch.qmc.sobol", ["botorch/qmc/sobol.pyx"])]
+
+setup(
+    name="botorch",
+    version="pre-alpha",
+    description="Bayesian Optimization in PyTorch",
+    author="Facebook, Inc.",
+    license="MIT",
+    url="https://github.com/facebookexternal/botorch",
+    keywords=["Bayesian Optimization", "pytorch", "Gaussian Process"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    setup_requires=["cython", "numpy"],  # TODO: Ship generated .c sources
+    install_requires=["gpytorch>=0.1.1", "scipy"],
+    include_dirs=[numpy.get_include()],
+    packages=find_packages(),
+    ext_modules=cythonize(extensions),
+)
