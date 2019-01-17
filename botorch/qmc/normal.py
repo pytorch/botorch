d9a1e44fd7fd59db079d2323c0367d9c34cbb791
Max Balandat <balandat@fb.com>
2019-01-17 14:48:04 -0800

    Fix NormalQMCEngine to properly sample from N(0, I_d)
    Summary:
    Fixing the Sobol dimension in the previous implementation was wrong.
    This corrects this using the algorithm from pg. 123 of:
    G. Pages. Numerical Probability: An Introduction with Applications to Finance. Universitext. Springer International Publishing, 2018.
    
    Reviewed By: bkarrer
    
    Differential Revision: D13718713
    
    fbshipit-source-id: cf646e85f8685ac29ce28556fc396326a416bf9a

--- botorch/qmc/normal.py
+++ botorch/qmc/normal.py
@@ -9,25 +9,38 @@ from .sobol import SobolEngine
 
 
 class NormalQMCEngine:
-    """Engine for drawing qMC samples from the standard Normal
+    """Engine for drawing qMC samples from a multivariate normal N(0, I_d)
 
     Args:
+        d: The dimension of the samples
         seed: The seed with which to seed the random number generator of the
             underlying SobolEngine.
 
-    This implementation uses Box-Muller transformed Sobol samples.
+    This implementation uses Box-Muller transformed Sobol samples following
+    pg. 123 in [1]
+
+    [1] G. Pages. Numerical Probability: An Introduction with Applications to Finance.
+        Universitext. Springer International Publishing, 2018.
+
     """
 
-    def __init__(self, seed: Optional[int] = None) -> None:
-        self._sobol_engine = SobolEngine(dimen=2, scramble=True, seed=seed)
+    def __init__(self, d: int, seed: Optional[int] = None) -> None:
+        self._d = d
+        self._seed = seed
+        # to apply Box-Muller, we need an even number of dimensions
+        sobol_dim = 2 * math.ceil(d / 2)
+        self._sobol_engine = SobolEngine(dimen=sobol_dim, scramble=True, seed=seed)
 
     def draw(self, n: int = 1) -> np.ndarray:
         """Draw n qMC samples from the standard Normal."""
         # get base samples
-        samples = self._sobol_engine.draw(math.ceil(n / 2))
+        samples = self._sobol_engine.draw(n)
         # apply Box-Muller transform
-        R = np.sqrt(-2 * np.log(samples[:, 0]))
-        theta = 2 * math.pi * samples[:, 1]
-        Z = np.concatenate([R * np.cos(theta), R * np.sin(theta)])
-        np.random.shuffle(Z)
-        return Z[:n]
+        even = np.arange(0, samples.shape[-1], 2)  # note: [1] indexes starting from 1
+        Rs = np.sqrt(-2 * np.log(samples[:, even]))
+        thetas = 2 * math.pi * samples[:, 1 + even]
+        cos = np.cos(thetas)
+        sin = np.sin(thetas)
+        transf_samples = np.stack([Rs * cos, Rs * sin], -1).reshape(n, -1)
+        # make sure we only return the number of dimension requested
+        return transf_samples[:, : self._d]
