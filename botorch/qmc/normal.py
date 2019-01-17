ecdc2c537fe0ec43c14a0f2041534f1355b79c6f
Max Balandat <balandat@fb.com>
2019-01-17 14:48:04 -0800

    Extend NormalQMCEngine to use inverse transform
    Summary: See title
    
    Reviewed By: bkarrer
    
    Differential Revision: D13720137
    
    fbshipit-source-id: 9af3e2eceb10fb9fd07285f881924b3155ae30e0

--- botorch/qmc/normal.py
+++ botorch/qmc/normal.py
@@ -4,6 +4,7 @@ import math
 from typing import Optional
 
 import numpy as np
+from scipy.stats import norm
 
 from .sobol import SobolEngine
 
@@ -15,32 +16,44 @@ class NormalQMCEngine:
         d: The dimension of the samples
         seed: The seed with which to seed the random number generator of the
             underlying SobolEngine.
+        inv_transform: If True, use inverse transform instead of Box-Muller
 
-    This implementation uses Box-Muller transformed Sobol samples following
-    pg. 123 in [1]
+    By default, this implementation uses Box-Muller transformed Sobol samples
+    following pg. 123 in [1]. To use the inverse transform instead, set
+    inv_transform to True.
 
     [1] G. Pages. Numerical Probability: An Introduction with Applications to Finance.
         Universitext. Springer International Publishing, 2018.
 
     """
 
-    def __init__(self, d: int, seed: Optional[int] = None) -> None:
+    def __init__(
+        self, d: int, seed: Optional[int] = None, inv_transform: bool = False
+    ) -> None:
         self._d = d
         self._seed = seed
-        # to apply Box-Muller, we need an even number of dimensions
-        sobol_dim = 2 * math.ceil(d / 2)
+        self._inv_transform = inv_transform
+        if inv_transform:
+            sobol_dim = d
+        else:
+            # to apply Box-Muller, we need an even number of dimensions
+            sobol_dim = 2 * math.ceil(d / 2)
         self._sobol_engine = SobolEngine(dimen=sobol_dim, scramble=True, seed=seed)
 
     def draw(self, n: int = 1) -> np.ndarray:
         """Draw n qMC samples from the standard Normal."""
         # get base samples
         samples = self._sobol_engine.draw(n)
-        # apply Box-Muller transform
-        even = np.arange(0, samples.shape[-1], 2)  # note: [1] indexes starting from 1
-        Rs = np.sqrt(-2 * np.log(samples[:, even]))
-        thetas = 2 * math.pi * samples[:, 1 + even]
-        cos = np.cos(thetas)
-        sin = np.sin(thetas)
-        transf_samples = np.stack([Rs * cos, Rs * sin], -1).reshape(n, -1)
-        # make sure we only return the number of dimension requested
-        return transf_samples[:, : self._d]
+        if self._inv_transform:
+            # apply inverse transform (values to close to 0/1 result in inf values)
+            return norm.ppf(0.5 + (1 - 1e-10) * (samples - 0.5))
+        else:
+            # apply Box-Muller transform (note: [1] indexes starting from 1)
+            even = np.arange(0, samples.shape[-1], 2)
+            Rs = np.sqrt(-2 * np.log(samples[:, even]))
+            thetas = 2 * math.pi * samples[:, 1 + even]
+            cos = np.cos(thetas)
+            sin = np.sin(thetas)
+            transf_samples = np.stack([Rs * cos, Rs * sin], -1).reshape(n, -1)
+            # make sure we only return the number of dimension requested
+            return transf_samples[:, : self._d]
