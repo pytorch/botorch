c8adfa47290bcd623130af12361af53925956d51
Max Balandat <balandat@fb.com>
2019-01-17 14:48:04 -0800

    Fix NormalQMCEngine to properly sample from N(0, I_d)
    Summary:
    Fixing the Sobol dimension in the previous implementation was wrong.
    This corrects this using the algorithm from pg. 123 of:
    G. Pages. Numerical Probability: An Introduction with Applications to Finance. Universitext. Springer International Publishing, 2018.
    
    Reviewed By: bkarrer
    
    Differential Revision: D13718713
    
    fbshipit-source-id: cf646e85f8685ac29ce28556fc396326a416bf9a

--- test/qmc/test_normal.py
+++ test/qmc/test_normal.py
@@ -4,37 +4,55 @@ import unittest
 
 import numpy as np
 from botorch.qmc import NormalQMCEngine
+from scipy.stats import shapiro
 
 
 class NormalQMCTests(unittest.TestCase):
     def testNormalQMCEngine(self):
-        engine = NormalQMCEngine()
+        # d = 1
+        engine = NormalQMCEngine(d=1)
         samples = engine.draw()
-        self.assertEqual(len(samples), 1)
+        self.assertEqual(samples.shape, (1, 1))
         samples = engine.draw(n=5)
-        self.assertEqual(len(samples), 5)
+        self.assertEqual(samples.shape, (5, 1))
+        # d = 2
+        engine = NormalQMCEngine(d=2)
+        samples = engine.draw()
+        self.assertEqual(samples.shape, (1, 2))
+        samples = engine.draw(n=5)
+        self.assertEqual(samples.shape, (5, 2))
 
     def testNormalQMCEngineSeeded(self):
-        engine = NormalQMCEngine(seed=1234)
+        # test even dimension
+        engine = NormalQMCEngine(d=2, seed=12345)
         samples = engine.draw(n=2)
-        samples_expected = np.array([-0.73137909, -0.64682878])
-        self.assertTrue(np.allclose(np.sort(samples), np.sort(samples_expected)))
+        samples_expected = np.array(
+            [[-0.63099602, -1.32950772], [0.29625805, 1.86425618]]
+        )
+        self.assertTrue(np.allclose(samples, samples_expected))
+        # test odd dimension
+        engine = NormalQMCEngine(d=3, seed=12345)
+        samples = engine.draw(n=2)
+        samples_expected = np.array(
+            [
+                [1.83169884, -1.40473647, 0.24334828],
+                [0.36596099, 1.2987395, -1.47556275],
+            ]
+        )
+        self.assertTrue(np.allclose(samples, samples_expected))
 
     def testNormalQMCEngineShapiro(self):
-        engine = NormalQMCEngine(seed=1234)
+        engine = NormalQMCEngine(d=2, seed=12345)
         samples = engine.draw(n=250)
-        # check mean and standard deviation
-        self.assertEqual(len(samples), 250)
-        self.assertLess(np.abs(samples.mean()), 1e-2)
-        self.assertLess(np.abs(samples.std() - 1), 1e-2)
-        # if scipy is available, perform Shapiro-Wilk test for normality
-        try:
-            from scipy.stats import shapiro
-
-            _, pval = shapiro(samples)
-            self.assertGreater(pval, 0.99)
-        except ModuleNotFoundError:
-            pass
+        self.assertTrue(all(np.abs(samples.mean(axis=0)) < 1e-2))
+        self.assertTrue(all(np.abs(samples.std(axis=0) - 1) < 1e-2))
+        # perform Shapiro-Wilk test for normality
+        for i in (0, 1):
+            _, pval = shapiro(samples[:, i])
+            self.assertGreater(pval, 0.9)
+        # make sure samples are uncorrelated
+        cov = np.cov(samples.transpose())
+        self.assertLess(np.abs(cov[0, 1]), 1e-2)
 
 
 if __name__ == "__main__":
