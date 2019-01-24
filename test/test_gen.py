c68d911b54397d218be140fd129046ac820611f0
Brian Karrer <briankarrer@fb.com>
2019-01-24 06:59:49 -0800

    fix fixed_features for scipy generation
    Summary: This adds unit tests for fixed features for scipy candidate generation, and fixes a bug where the gradient was computed improperly when using fixed features.
    
    Reviewed By: Balandat
    
    Differential Revision: D13517500
    
    fbshipit-source-id: b6587e6caf34c626d2973594e817c1a7c70092fc

--- test/test_gen.py
+++ test/test_gen.py
@@ -14,7 +14,7 @@ from .test_fit import NOISE
 
 
 class TestBaseCandidateGeneration(unittest.TestCase):
-    def _setUp(self, double=False, cuda=False):
+    def _setUp(self, double=False, cuda=False, expand=False):
         device = torch.device("cuda") if cuda else torch.device("cpu")
         dtype = torch.double if double else torch.float
         train_x = torch.linspace(0, 1, 10, device=device, dtype=dtype)
@@ -22,8 +22,15 @@ class TestBaseCandidateGeneration(unittest.TestCase):
         noise = torch.tensor(NOISE, device=device, dtype=dtype)
         self.train_x = train_x.unsqueeze(-1)
         self.train_y = train_y + noise
+
+        if expand:
+            self.train_x = self.train_x.expand(-1, 2)
+            self.initial_candidates = torch.tensor(
+                [[0.5, 1.0]], device=device, dtype=dtype
+            )
+        else:
+            self.initial_candidates = torch.tensor([[0.5]], device=device, dtype=dtype)
         self.f_best = self.train_y.max().item()
-        self.initial_candidates = torch.tensor([[0.5]], device=device, dtype=dtype)
         self.likelihood = GaussianLikelihood()
         model = SingleTaskGP(self.train_x, self.train_y, self.likelihood)
         self.model = model.to(device=device, dtype=dtype)
@@ -32,11 +39,6 @@ class TestBaseCandidateGeneration(unittest.TestCase):
 
 
 class TestGenCandidates(TestBaseCandidateGeneration):
-    def _setUp(self, double=False, cuda=False):
-        super()._setUp(double=double, cuda=cuda)
-        self.f_best = self.train_y.max().item()
-        self.initial_candidates = torch.tensor([[0.5]]).type_as(self.train_x)
-
     def test_gen_candidates_scipy(self, cuda=False):
         for double in (True, False):
             self._setUp(double=double, cuda=cuda)
@@ -52,6 +54,44 @@ class TestGenCandidates(TestBaseCandidateGeneration):
     def test_gen_candidates_scipy_cuda(self):
         if torch.cuda.is_available():
             self.test_gen_candidates_scipy(cuda=True)
+
+    def test_gen_candidates_scipy_with_none_fixed_features(self, cuda=False):
+        for double in (True, False):
+            self._setUp(double=double, cuda=cuda, expand=True)
+            qEI = qExpectedImprovement(self.model, best_f=self.f_best)
+            candidates, _ = gen_candidates_scipy(
+                initial_candidates=self.initial_candidates,
+                acquisition_function=qEI,
+                lower_bounds=0,
+                upper_bounds=1,
+                fixed_features={1: None},
+            )
+            candidates = candidates.squeeze(0)
+            self.assertTrue(0 <= candidates[0] <= 1)
+            self.assertTrue(candidates[1].item() == 1.0)
+
+    def test_gen_candidates_scipy_with_none_fixed_features_cuda(self):
+        if torch.cuda.is_available():
+            self.test_gen_candidates_scipy_with_none_fixed_features(cuda=True)
+
+    def test_gen_candidates_scipy_with_fixed_features(self, cuda=False):
+        for double in (True, False):
+            self._setUp(double=double, cuda=cuda, expand=True)
+            qEI = qExpectedImprovement(self.model, best_f=self.f_best)
+            candidates, _ = gen_candidates_scipy(
+                initial_candidates=self.initial_candidates,
+                acquisition_function=qEI,
+                lower_bounds=0,
+                upper_bounds=1,
+                fixed_features={1: 0.25},
+            )
+            candidates = candidates.squeeze(0)
+            self.assertTrue(0 <= candidates[0] <= 1)
+            self.assertTrue(candidates[1].item() == 0.25)
+
+    def test_gen_candidates_scipy_with_fixed_features_cuda(self, cuda=False):
+        if torch.cuda.is_available():
+            self.test_gen_candidates_scipy_with_fixed_features(cuda=True)
 
 
 if __name__ == "__main__":
