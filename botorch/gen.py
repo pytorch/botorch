Fix gradient issue in fix_features
Summary:
Autograd did not get along with the `fill_` call in fix feature.
Alos, if we don't modify the tensor in-place, we have to do call fix_features once more in `gen_candidates`.

Reviewed By: sdaulton

Differential Revision: D13792864

fbshipit-source-id: c60c088080bcb62b30add444825ed59c7c80d354

botorch/gen.py
@@ -95,8 +95,9 @@ def gen_candidates_scipy(
         options=options,
     )
 
-    candidates = (
-        torch.from_numpy(res.x).type_as(initial_candidates).view(shapeX).contiguous()
+    candidates = fix_features(
+        X=torch.from_numpy(res.x).type_as(initial_candidates).view(shapeX).contiguous(),
+        fixed_features=fixed_features,
     )
     batch_acquisition = acquisition_function(candidates)
     return candidates, batch_acquisition

botorch/utils.py
@@ -31,11 +31,9 @@ def check_convergence(
 
 
 def _fix_feature(Z: Tensor, value: Optional[float]) -> Tensor:
-    Z_detached = Z.detach().requires_grad_(False)
     if value is None:
-        return Z_detached
-    else:
-        return Z_detached.fill_(value)
+        return Z.detach()
+    return torch.full_like(Z, value)
 
 
 def fix_features(