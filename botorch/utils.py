b9fce5c026a2d0e02e943a967728b78e569f1cce
Max Balandat <balandat@fb.com>
2019-02-07 10:57:28 -0800

    Utility for generating constraint callables from constraint matrices
    Summary: See title.
    
    Reviewed By: sdaulton
    
    Differential Revision: D13518545
    
    fbshipit-source-id: 9207cb3324920683b4e424d00353ea63a799a617

--- botorch/utils.py
+++ botorch/utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from contextlib import contextmanager
-from typing import Callable, Dict, Generator, List, Optional, Union
+from functools import partial
+from typing import Callable, Dict, Generator, List, Optional, Tuple, Union
 
 import torch
 from botorch.qmc.normal import NormalQMCEngine
@@ -257,6 +258,35 @@ def draw_sobol_normal_samples(
         dtype=torch.float if dtype is None else dtype,
         device=device,  # None here will leave it on the cpu
     )
+
+
+def get_outcome_constraint_transforms(
+    outcome_constraints: Optional[Tuple[Tensor, Tensor]],
+) -> Optional[List[Callable[[Tensor], Tensor]]]:
+    """
+    Create outcome constraint callables from outcome constraint tensors
+
+    Args:
+        outcome_constraints: A tuple of `(A, b)`. For `k` outcome constraints
+            and `m` outputs at `f(x)``, `A` is `k x m` and `b` is `k x 1` such
+            that `A f(x) <= b`.
+
+    Returns:
+        A list of callables, each mapping a Tensor of size `b x q x t` to a
+        Tensor of size `b x q`, where `t` is the number of outputs (tasks) of
+        the model. Negative values imply feasibility. The callables support
+        broadcasting (e.g. for calling on a tensor of shape
+        `mc_samples x b x q x t`).
+
+    """
+    if outcome_constraints is None:
+        return None
+    A, b = outcome_constraints
+
+    def oc(a: Tensor, rhs: Tensor, Y: Tensor) -> Tensor:
+        return torch.sum(Y * a.view(1, 1, -1), dim=-1) - rhs
+
+    return [partial(oc, a, rhs) for a, rhs in zip(A, b)]
 
 
 def standardize(X: Tensor) -> Tensor:
--- test/test_utils.py
+++ test/test_utils.py
@@ -9,6 +9,7 @@ from botorch.utils import (
     fix_features,
     gen_x_uniform,
     get_objective_weights_transform,
+    get_outcome_constraint_transforms,
     manual_seed,
     standardize,
 )
@@ -193,6 +194,50 @@ class TestGetObjectiveWeightsTransform(unittest.TestCase):
         objective_transform = get_objective_weights_transform(torch.tensor([1.0, 1.0]))
         X_transformed = objective_transform(X)
         self.assertTrue(torch.equal(torch.sum(X, dim=-1), X_transformed))
+
+
+class TestGetOutcomeConstraintTransform(unittest.TestCase):
+    def setUp(self):
+        self.A = torch.tensor([[-1.0, 0.0, 0.0], [0.0, 1.0, 1.0]])
+        self.b = torch.tensor([[-0.5], [1.0]])
+        self.Ys = torch.tensor([[0.75, 1.0, 0.5], [0.25, 1.5, 1.0]])
+        self.results = torch.tensor([[-0.25, 0.5], [0.25, 1.5]]).view(2, 2, 1, 1)
+
+    def testNone(self):
+        self.assertIsNone(get_outcome_constraint_transforms(None))
+
+    def testBasicEvaluation(self, cuda=False):
+        device = torch.device("cuda") if cuda else torch.device("cpu")
+        for dtype in (torch.float, torch.double):
+            A = self.A.to(dtype=dtype, device=device)
+            b = self.b.to(dtype=dtype, device=device)
+            Ys = self.Ys.to(dtype=dtype, device=device)
+            results = self.results.to(dtype=dtype, device=device)
+            ocs = get_outcome_constraint_transforms((A, b))
+            self.assertEqual(len(ocs), 2)
+            for i in (0, 1):
+                for j in (0, 1):
+                    self.assertTrue(torch.equal(ocs[j](Ys[i]), results[i, j]))
+
+    def testBasicEvaluation_cuda(self):
+        if torch.cuda.is_available():
+            self.testBasicEvaluation(cuda=True)
+
+    def testBroadcastEvaluation(self, cuda=False):
+        device = torch.device("cuda") if cuda else torch.device("cpu")
+        k, t = 3, 4
+        mc_samples, b, q = 6, 4, 5
+        for dtype in (torch.float, torch.double):
+            A_ = torch.randn(k, t, dtype=dtype, device=device)
+            b_ = torch.randn(k, 1, dtype=dtype, device=device)
+            Y = torch.randn(mc_samples, b, q, t, dtype=dtype, device=device)
+            ocs = get_outcome_constraint_transforms((A_, b_))
+            self.assertEqual(len(ocs), k)
+            self.assertEqual(ocs[0](Y).shape, torch.Size([mc_samples, b, q]))
+
+    def testBroadcastEvaluation_cuda(self):
+        if torch.cuda.is_available():
+            self.testBroadcastEvaluation(cuda=True)
 
 
 class TestStandardize(unittest.TestCase):
