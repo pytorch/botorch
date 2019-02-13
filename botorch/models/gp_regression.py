Fix noise level in SingleTaskGP if variance is close to constant
Summary: SingleTaskGP in botorch by default estimates the noise level. On the AE side, we currently always have known noise. Rather than have to use the full heteroskedastic model for every fit, this allows a known homoskedastic noise level to be fixed in the SingleTaskGP, and makes that the default on the AE side if the variance in observed noise levels is relatively low.

Reviewed By: Balandat

Differential Revision: D14060091

fbshipit-source-id: e1b34adb6b94a6627cb477397247667b2c4c5527

botorch/models/gp_regression.py
@@ -35,7 +35,11 @@ class SingleTaskGP(ExactGP, GPyTorchModel):
     """
 
     def __init__(
-        self, train_X: Tensor, train_Y: Tensor, likelihood: Optional[Likelihood] = None
+        self,
+        train_X: Tensor,
+        train_Y: Tensor,
+        likelihood: Optional[Likelihood] = None,
+        fixed_noise_variance: Optional[float] = None,
     ) -> None:
         if train_X.ndimension() == 1:
             batch_size, ard_num_dims = 1, None
@@ -49,7 +53,13 @@ class SingleTaskGP(ExactGP, GPyTorchModel):
             likelihood = GaussianLikelihood(
                 noise_prior=GammaPrior(0.1, 0.01), batch_size=batch_size
             )
-            likelihood.parameter_bounds = {"noise_covar.raw_noise": (-15, None)}
+            if fixed_noise_variance is not None:
+                likelihood.noise = torch.full_like(
+                    likelihood.noise, fixed_noise_variance
+                )
+                likelihood.raw_noise.requires_grad_(False)
+            else:
+                likelihood.parameter_bounds = {"noise_covar.raw_noise": (-15, None)}
         else:
             self._likelihood_state_dict = deepcopy(likelihood.state_dict())
         super().__init__(train_X, train_Y, likelihood)