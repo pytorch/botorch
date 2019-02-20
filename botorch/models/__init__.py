15d71561f662503f7e103a7745b78b3cea2c5697
Max Balandat <balandat@fb.com>
2019-02-20 11:15:02 -0800

    ConstantNoiseGP
    Summary: A GP that uses a fixed noise level.
    
    Reviewed By: bletham
    
    Differential Revision: D14095380
    
    fbshipit-source-id: e5430a54cd0f59136cb09fe8e7a8d37842030652

--- botorch/models/__init__.py
+++ botorch/models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from .constant_noise import ConstantNoiseGP
 from .fidelity_aware import FidelityAwareSingleTaskGP
 from .gp_regression import HeteroskedasticSingleTaskGP, SingleTaskGP
 from .model import Model
@@ -7,6 +8,7 @@ from .multi_output_gp_regression import MultiOutputGP
 
 
 __all__ = [
+    ConstantNoiseGP,
     FidelityAwareSingleTaskGP,
     HeteroskedasticSingleTaskGP,
     Model,
