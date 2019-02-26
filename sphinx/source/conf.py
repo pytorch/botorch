9256e251b8b4b84633305c66065d967d762c33ca
Max Balandat <balandat@fb.com>
2019-02-26 14:48:19 -0800

    Get rid of ref to non-existent _static dir
    Summary:
    For now we don't have any files to track, so let's get rid of that option.
    Removes the following warning when building:
    `WARNING: html_static_path entry 'path/to/botorch/sphinx/source/_static' does not exist`
    
    Reviewed By: danielrjiang
    
    Differential Revision: D14228812
    
    fbshipit-source-id: c7023baa0568c57530fbed9f7382ba76f99e4b8e

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -79,7 +79,8 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+html_static_path = []  # for now we have no static files to track
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
