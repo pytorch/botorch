e6e39409b35e9e4cfcb4d02a717b29d603aa809f
Max Balandat <balandat@fb.com>
2019-03-16 09:27:52 -0700

    Move FidelityAware GPs into botorch_fb
    Summary: These aren't very well tested, we can move them back in when in a better state.
    
    Reviewed By: sdaulton
    
    Differential Revision: D14494400
    
    fbshipit-source-id: 881a43fd73ec4ef51113979e393884e48e6b92b2

--- botorch/models/__init__.py
+++ botorch/models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from .constant_noise import ConstantNoiseGP
-from .fidelity_aware import FidelityAwareSingleTaskGP
 from .gp_regression import HeteroskedasticSingleTaskGP, SingleTaskGP
 from .model import Model
 from .multi_output_gp_regression import MultiOutputGP
@@ -9,7 +8,6 @@ from .multi_output_gp_regression import MultiOutputGP
 
 __all__ = [
     "ConstantNoiseGP",
-    "FidelityAwareSingleTaskGP",
     "HeteroskedasticSingleTaskGP",
     "Model",
     "MultiOutputGP",
