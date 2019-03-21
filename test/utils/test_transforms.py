3ff7819de67d170d455ea77359e63744b859ce2f
Sam Daulton <samdaulton@instagram.com>
2019-03-21 11:46:11 -0700

    Meta learning with RGPE
    Summary:
    Tutorial using a custom gpytorch model (RGPE) to do meta-learning across related tasks (here a toy 1-D function).
    
    One difference from the paper, which I note in the tutorial, is that I refit the kernel hyperparameters for each fold of LOOCV (whereas the paper re-uses the kernel hyperparameters obtained from fitting a GP on all the data points). I can update this, but I think it provides a nice example of how to do the LOOCV in batch mode in gpy/botorch.
    
    Reviewed By: Balandat
    
    Differential Revision: D14552196
    
    fbshipit-source-id: 2d154400ef034cd5e091a65e276ec93a6d88e6ab

--- test/utils/test_transforms.py
+++ test/utils/test_transforms.py
@@ -4,21 +4,57 @@ import unittest
 
 import torch
 from botorch.acquisition.utils import batch_mode_transform, match_batch_shape
-from botorch.utils.transforms import standardize
+from botorch.utils.transforms import normalize, standardize, unnormalize
 from torch import Tensor
 
 
 class TestStandardize(unittest.TestCase):
-    def test_standardize(self):
-        X = torch.tensor([0.0, 0.0])
-        self.assertTrue(torch.equal(X, standardize(X)))
-        X2 = torch.tensor([0.0, 1.0, 1.0, 1.0])
-        expected_X2_stdized = torch.tensor([-1.5, 0.5, 0.5, 0.5])
-        self.assertTrue(torch.equal(expected_X2_stdized, standardize(X2)))
-        X3 = torch.tensor([[0.0, 1.0, 1.0, 1.0], [0.0, 0.0, 0.0, 0.0]]).transpose(1, 0)
-        X3_stdized = standardize(X3)
-        self.assertTrue(torch.equal(X3_stdized[:, 0], expected_X2_stdized))
-        self.assertTrue(torch.equal(X3_stdized[:, 1], torch.zeros(4)))
+    def test_standardize(self, cuda=False):
+        tkwargs = {"device": torch.device("cuda" if cuda else "cpu")}
+        for dtype in (torch.float, torch.double):
+            tkwargs["dtype"] = dtype
+            X = torch.tensor([0.0, 0.0], **tkwargs)
+            self.assertTrue(torch.equal(X, standardize(X)))
+            X2 = torch.tensor([0.0, 1.0, 1.0, 1.0], **tkwargs)
+            expected_X2_stdized = torch.tensor([-1.5, 0.5, 0.5, 0.5], **tkwargs)
+            self.assertTrue(torch.equal(expected_X2_stdized, standardize(X2)))
+            X3 = torch.tensor(
+                [[0.0, 1.0, 1.0, 1.0], [0.0, 0.0, 0.0, 0.0]], **tkwargs
+            ).transpose(1, 0)
+            X3_stdized = standardize(X3)
+            self.assertTrue(torch.equal(X3_stdized[:, 0], expected_X2_stdized))
+            self.assertTrue(torch.equal(X3_stdized[:, 1], torch.zeros(4, **tkwargs)))
+
+    def test_standardize_cuda(self):
+        if torch.cuda.is_available():
+            self.test_standardize(cuda=True)
+
+
+class TestNormalizeAndUnnormalize(unittest.TestCase):
+    def test_normalize_unnormalize(self, cuda=False):
+        tkwargs = {"device": torch.device("cuda" if cuda else "cpu")}
+        for dtype in (torch.float, torch.double):
+            tkwargs["dtype"] = dtype
+            X = torch.tensor([0.0, 0.25, 0.5], **tkwargs).view(-1, 1)
+            expected_X_normalized = torch.tensor([0.0, 0.5, 1.0], **tkwargs).view(-1, 1)
+            bounds = torch.tensor([0.0, 0.5], **tkwargs).view(-1, 1)
+            X_normalized = normalize(X, bounds=bounds)
+            self.assertTrue(torch.equal(expected_X_normalized, X_normalized))
+            self.assertTrue(torch.equal(X, unnormalize(X_normalized, bounds=bounds)))
+            X2 = torch.tensor(
+                [[0.25, 0.125, 0.0], [0.25, 0.0, 0.5]], **tkwargs
+            ).transpose(1, 0)
+            expected_X2_normalized = torch.tensor(
+                [[1.0, 0.5, 0.0], [0.5, 0.0, 1.0]], **tkwargs
+            ).transpose(1, 0)
+            bounds2 = torch.tensor([[0.0, 0.0], [0.25, 0.5]], **tkwargs)
+            X2_normalized = normalize(X2, bounds=bounds2)
+            self.assertTrue(torch.equal(X2_normalized, expected_X2_normalized))
+            self.assertTrue(torch.equal(X2, unnormalize(X2_normalized, bounds=bounds2)))
+
+    def test_normalize_unnormalize_cuda(self):
+        if torch.cuda.is_available():
+            self.test_normalize_unnormalize(cuda=True)
 
 
 class BMIMTestClass:
