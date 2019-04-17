0ea784839e5aa2a410d8cabb99d125b029ebb935
Sam Daulton <samdaulton@instagram.com>
2019-04-17 09:42:17 -0700

    batch mode independent multioutput gp
    Summary:
    - Make all single task models support multiple outputs modeling outputs independently using a batch for each output.
    
    Diffing out for visibility and initial feedback as I fix the tests.
    
    Reviewed By: Balandat
    
    Differential Revision: D14953851
    
    fbshipit-source-id: 1876ee3ae85b230063d231a70a04274d46219f30

--- botorch/models/gpytorch.py
+++ botorch/models/gpytorch.py
@@ -11,13 +11,15 @@ from abc import ABC, abstractproperty
 from contextlib import ExitStack
 from typing import Any, List, Optional
 
+import torch
 from gpytorch import settings
-from gpytorch.distributions import MultitaskMultivariateNormal
+from gpytorch.distributions import MultitaskMultivariateNormal, MultivariateNormal
+from gpytorch.lazy import lazify
 from torch import Tensor
 
 from ..posteriors.gpytorch import GPyTorchPosterior
 from .model import Model
-from .utils import _make_X_full
+from .utils import _make_X_full, add_output_dim
 
 
 class GPyTorchModel(Model, ABC):
@@ -37,9 +39,8 @@ class GPyTorchModel(Model, ABC):
         r"""Computes the posterior over model outputs at the provided points.
 
         Args:
-            X: A `b x q x d`-dim Tensor, where `d` is the dimension of the
-                feature space, `q` is the number of points considered jointly,
-                and `b` is the batch dimension.
+            X: A `(batch_shape) x q x d`-dim Tensor, where `d` is the dimension of the
+                feature space and `q` is the number of points considered jointly.
             output_indices: A list of indices, corresponding to the outputs over
                 which to compute the posterior (if the model is multi-output).
                 Can be used to speed up computation if only a subset of the
@@ -71,6 +72,91 @@ class GPyTorchModel(Model, ABC):
             if observation_noise:
                 # TODO: Allow passing in observation noise via kwarg
                 mvn = self.likelihood(mvn, X)
+            return GPyTorchPosterior(mvn=mvn)
+
+
+class BatchedMultiOutputGPyTorchModel(GPyTorchModel):
+    r"""Base class for batched multi-output GPyTorch models with independent outputs.
+
+    This model should be used when the same training data is used for all outputs.
+    Outputs are modeled independently by using a different batch for each output.
+    """
+
+    _num_outputs: int
+    _input_batch_shape: torch.Size
+    _aug_batch_shape: torch.Size
+
+    def _set_dimensions(self, train_X: Tensor, train_Y: Tensor) -> None:
+        r"""Store the number of outputs and the batch shape.
+
+        Args:
+            train_X: A `n x d` or `batch_shape x n x d` (batch mode) tensor of training
+                features.
+            train_Y: A `n x (o)` or `batch_shape x n x (o)` (batch mode) tensor of
+                training observations.
+        """
+        self._num_outputs = train_Y.shape[-1] if train_Y.dim() == train_X.dim() else 1
+        self._input_batch_shape = train_X.shape[:-2]
+        if self._num_outputs > 1:
+            self._aug_batch_shape = (
+                torch.Size([self._num_outputs]) + self._input_batch_shape
+            )
+        else:
+            self._aug_batch_shape = self._input_batch_shape
+
+    def posterior(
+        self,
+        X: Tensor,
+        output_indices: Optional[List[int]] = None,
+        observation_noise: bool = False,
+        **kwargs: Any,
+    ) -> GPyTorchPosterior:
+        r"""Computes the posterior over model outputs at the provided points.
+
+        Args:
+            X: A `(batch_shape) x q x d`-dim Tensor, where `d` is the dimension of the
+                feature space and `q` is the number of points considered jointly.
+            output_indices: A list of indices, corresponding to the outputs over
+                which to compute the posterior (if the model is multi-output).
+                Can be used to speed up computation if only a subset of the
+                model's outputs are required for optimization. If omitted,
+                computes the posterior over all model outputs.
+            observation_noise: If True, add observation noise to the posterior.
+            detach_test_caches: If True, detach GPyTorch test caches during
+                computation of the posterior. Required for being able to compute
+                derivatives with respect to training inputs at test time (used
+                e.g. by qNoisyExpectedImprovement). Defaults to `True`.
+
+        Returns:
+            A `GPyTorchPosterior` object, representing `batch_shape` joint
+            distributions over `q` points and the outputs selected by
+            `output_indices` each. Includes observation noise if
+            `observation_noise=True`.
+        """
+        self.eval()  # make sure model is in eval mode
+        detach_test_caches = kwargs.get("detach_test_caches", True)
+        with ExitStack() as es:
+            es.enter_context(settings.debug(False))
+            es.enter_context(settings.fast_pred_var())
+            es.enter_context(settings.detach_test_caches(detach_test_caches))
+            # insert a dimension for the output dimension
+            if self._num_outputs > 1:
+                X, output_dim_idx = add_output_dim(
+                    X=X, original_batch_shape=self._input_batch_shape
+                )
+            mvn = self(X)
+            mean_x = mvn.mean
+            covar_x = mvn.covariance_matrix
+            if self._num_outputs > 1:
+                output_indices = output_indices or range(self._num_outputs)
+                mvns = [
+                    MultivariateNormal(
+                        mean_x.select(dim=output_dim_idx, index=t),
+                        lazify(covar_x.select(dim=output_dim_idx, index=t)),
+                    )
+                    for t in output_indices
+                ]
+                mvn = MultitaskMultivariateNormal.from_independent_mvns(mvns=mvns)
         return GPyTorchPosterior(mvn=mvn)
 
 
@@ -111,7 +197,7 @@ class MultiOutputGPyTorchModel(GPyTorchModel, ABC):
                 e.g. by qNoisyExpectedImprovement).
 
         Returns:
-            A `GPyTorchPosterior` object, representing a batch of `b` joint
+            A `GPyTorchPosterior` object, representing `batch_shape` joint
             distributions over `q` points and the outputs selected by
             `output_indices` each. Includes measurement noise if
             `observation_noise=True`.
@@ -159,10 +245,9 @@ class MultiTaskGPyTorchModel(GPyTorchModel, ABC):
         r"""Computes the posterior over model outputs at the provided points.
 
         Args:
-            X: A `q x d` or `b x q x d` (batch mode) tensor, where `d` is the
-                dimension of the feature space (not including task indices),
-                `q` is the number of points considered jointly, and `b` is the
-                (optional) batch dimension.
+            X: A `q x d` or `batch_shape x q x d` (batch mode) tensor, where `d` is the
+                dimension of the feature space (not including task indices) and
+                `q` is the number of points considered jointly.
             output_indices: A list of indices, corresponding to the outputs over
                 which to compute the posterior (if the model is multi-output).
                 Can be used to speed up computation if only a subset of the
@@ -175,7 +260,7 @@ class MultiTaskGPyTorchModel(GPyTorchModel, ABC):
                 e.g. by qNoisyExpectedImprovement).
 
         Returns:
-            A `GPyTorchPosterior` object, representing a batch of `b` joint
+            A `GPyTorchPosterior` object, representing `batch_shape` joint
             distributions over `q` points and the outputs selected by
             `output_indices`. Includes measurement noise if
             `observation_noise=True`.
--- botorch/models/utils.py
+++ botorch/models/utils.py
@@ -4,7 +4,7 @@ r"""
 Utiltiy functions for models.
 """
 
-from typing import List
+from typing import List, Optional, Tuple
 
 import torch
 from torch import Tensor
@@ -31,3 +31,78 @@ def _make_X_full(X: Tensor, output_indices: List[int], tf: int) -> Tensor:
     return torch.cat(
         [torch.cat([X_l, indexer, X_r], dim=-1) for indexer in indexers], dim=0
     )
+
+
+def multioutput_to_batch_mode_transform(
+    train_X: Tensor,
+    train_Y: Tensor,
+    num_outputs: int,
+    train_Yvar: Optional[Tensor] = None,
+) -> Tuple[Tensor, Tensor, Optional[Tensor]]:
+    r"""Transforms training inputs for a multi-output model.
+
+    Used for multi-output models that internally are represented by a
+    batched single output model, where each output is modeled as an
+    independent batch.
+
+    Args:
+        train_X: A `n x d` or `batch_shape x n x d` (batch mode) tensor of training
+            features.
+        train_Y: A `n x (o)` or `batch_shape x n x (o)` (batch mode) tensor of
+            training observations.
+        num_outputs: number of outputs
+        train_Yvar: A `batch_shape x n x (o)`
+            tensor of observed measurement noise.
+
+    Returns:
+        3-element tuple containing
+
+        - A `(o) x batch_shape x n x d` tensor of training features.
+        - A `(o) x batch_shape x n` tensor of training observations.
+        - A `(o) x batch_shape x n` tensor observed measurement noise.
+    """
+    if num_outputs > 1:
+        # make train_Y `t x batch_shape x n`
+        train_Y = train_Y.permute(-1, *range(train_Y.dim() - 1))
+        # expand train_X to `t x batch_shape x n x d`
+        train_X = train_X.unsqueeze(0).expand(
+            torch.Size([num_outputs] + [-1] * train_X.dim())
+        )
+        if train_Yvar is not None:
+            # make train_Yvar `t x batch_shape x n`
+            train_Yvar = train_Yvar.permute(-1, *range(train_Yvar.dim() - 1))
+    elif train_Y.dim() > 1:
+        # t = 1, make train_Y `n`-dim
+        train_Y = train_Y.view(-1)
+        if train_Yvar is not None:
+            # expand train_Yvar `t x batch_shape x n`
+            train_Yvar = train_Yvar.view(-1)
+    return train_X, train_Y, train_Yvar
+
+
+def add_output_dim(X: Tensor, original_batch_shape: torch.Size) -> Tuple[Tensor, int]:
+    r"""Inserts the output dimension at the correct location. The trailing batch dimensions
+        of X must match the original batch dimensions of the training inputs, but
+        can also include extra batch dimensions.
+
+    Args:
+        X: A `(new_batch_shape) x (original_batch_shape) x n x d` tensor of features.
+        original_batch_shape: the batch shape of the model's training inputs.
+
+    Returns:
+        2-element tuple containing
+
+        - A `(new_batch_shape) x o x (original_batch_shape) x n x d` tensor of
+        features.
+        - The index corresponding to the output dimension.
+    """
+    num_original_batch_dims = len(original_batch_shape)
+    if X.shape[-(num_original_batch_dims + 2) : -2] != original_batch_shape:
+        raise ValueError(
+            "The trailing batch dimensions of X must match the batch dimensions of the"
+            " training inputs."
+        )
+    # insert `t` dimension
+    output_dim_idx = len(X.shape) - (num_original_batch_dims + 2)
+    X = X.unsqueeze(output_dim_idx)
+    return X, output_dim_idx
