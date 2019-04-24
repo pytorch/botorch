f5b7bff4ab9e2a5a23741fb4f285547221ec2c6e
Max Balandat <balandat@fb.com>
2019-04-24 10:24:41 -0700

    Add `__version__` attribute to botorch package (#45)
    Summary:
    This uses regexp parsing to get the version from setup.py so we have a single source.
    Pull Request resolved: https://github.com/facebookexternal/botorch/pull/45
    
    Differential Revision: D14892769
    
    fbshipit-source-id: 6475006d4f93ec0cf0fbcc641db91bc71ebf6e2a

--- botorch/__init__.py
+++ botorch/__init__.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import os
+import re
+
 from . import acquisition, exceptions, models, optim, posteriors, test_functions
 from .cross_validation import batch_cross_validation
 from .fit import fit_gpytorch_model
 from .gen import gen_candidates_scipy, gen_candidates_torch, get_best_candidates
 from .utils import manual_seed
+
+
+# get version string from setup.py
+with open(os.path.join(os.path.dirname(__file__), os.pardir, "setup.py"), "r") as f:
+    __version__ = re.search(r"version=['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
 
 
 __all__ = [
