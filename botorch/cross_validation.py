91f286b6bb3c70da5189b3cba5e68601e442591e
Sam Daulton <samdaulton@instagram.com>
2019-04-24 10:04:32 -0700

    update cross validation utility (#87)
    Summary:
    Pull Request resolved: https://github.com/facebookexternal/botorch/pull/87
    
    - Expand support to all GPyTorchModels (except multi-task which do not support batched training inputs currently)
    - Add support for LOOCV on training tensors with arbitrary batch shapes
    
    Reviewed By: Balandat
    
    Differential Revision: D15060512
    
    fbshipit-source-id: 4964d8f2fdd4e0a12ce51ded83a106da4cd54890

--- botorch/cross_validation.py
+++ botorch/cross_validation.py
@@ -7,95 +7,143 @@ Cross-validation utilities using batch evaluation mode.
 from typing import Any, Dict, NamedTuple, Optional, Type
 
 import torch
-from gpytorch.likelihoods import GaussianLikelihood
-from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
-from gpytorch.models import ExactGP
+from gpytorch.mlls.marginal_log_likelihood import MarginalLogLikelihood
 from torch import Tensor
-from torch.distributions import Distribution
 
 from .fit import fit_gpytorch_model
+from .models.gpytorch import GPyTorchModel
+from .optim.utils import _filter_kwargs
+from .posteriors.gpytorch import GPyTorchPosterior
 
 
 class CVFolds(NamedTuple):
-    train_x: Tensor
-    train_y: Tensor
-    test_x: Tensor
-    test_y: Tensor
+    train_X: Tensor
+    test_X: Tensor
+    train_Y: Tensor
+    test_Y: Tensor
+    train_Yvar: Optional[Tensor] = None
+    test_Yvar: Optional[Tensor] = None
 
 
 class CVResults(NamedTuple):
-    posterior: Distribution
-    observed: Tensor
+    model: GPyTorchModel
+    posterior: GPyTorchPosterior
+    observed_Y: Tensor
+    observed_Yvar: Optional[Tensor] = None
 
 
-def gen_loo_cv_folds(train_x: Tensor, train_y: Tensor) -> CVFolds:
-    r"""Generate LOO CV folds.
+def gen_loo_cv_folds(
+    train_X: Tensor, train_Y: Tensor, train_Yvar: Optional[Tensor] = None
+) -> CVFolds:
+    r"""Generate LOO CV folds w.r.t. to `n`.
 
     Args:
-        train_x: An `n x p` tensor
-        train_y: An `n` or `n x t` tensor (the latter n the multi-output case,
-            where `t` the number of outputs)
+        train_X: A `n x d` or `batch_shape x n x d` (batch mode) tensor of training
+            features.
+        train_Y: A `n x (o)` or `batch_shape x n x (o)` (batch mode) tensor of
+            training observations.
+        train_Yvar: A `batch_shape x n x (o)` or `batch_shape x n x (o)`
+            (batch mode) tensor of observed measurement noise.
 
     Returns:
         CVFolds tuple with the following fields
 
-        - train_x: A `n x (n-1) x p` tensor of training features
-        - train_y: A `n x (n-1)` (or `n x (n-1) x t`) tensor of training
-        observations
-        - test_x: A `n x 1 x p` tensor of test features
-        - test_y: A `n` or `n x t` tensor of test observations
+        - train_X: A `n x (n-1) x d` or `batch_shape x n x (n-1) x d` tensor of
+          training features.
+        - test_X: A `n x 1 x d` or `batch_shape x n x 1 x d` tensor of test features.
+        - train_Y: A `n x (n-1) x o` or `batch_shape x n x (n-1) x o` tensor of
+          training observations.
+        - test_Y: A `n x 1 x o` or `batch_shape x n x 1 x o` tensor of test
+          observations.
+        - train_Yvar: A `n x (n-1) x o` or `batch_shape x n x (n-1) x o` tensor
+          of observed measurement noise.
+        - test_Yvar: A `n x 1 x o` or `batch_shape x n x 1 x o` tensor of observed
+          measurement noise.
+
     """
-    masks = torch.eye(len(train_x), dtype=torch.uint8, device=train_x.device)
-    train_x_cv = torch.cat([train_x[~m].unsqueeze(0) for m in masks])
-    train_y_cv = torch.cat([train_y[~m].unsqueeze(0) for m in masks])
-    test_x_cv = torch.cat([train_x[m].unsqueeze(0) for m in masks])
-    test_y_cv = torch.cat([train_y[m].unsqueeze(0) for m in masks])
+    masks = torch.eye(train_X.shape[-2], dtype=torch.uint8, device=train_X.device)
+    if train_Y.dim() < train_X.dim():
+        # add output dimension
+        train_Y = train_Y.unsqueeze(-1)
+        if train_Yvar is not None:
+            train_Yvar = train_Yvar.unsqueeze(-1)
+    train_X_cv = torch.cat(
+        [train_X[..., ~m, :].unsqueeze(dim=-3) for m in masks], dim=-3
+    )
+    test_X_cv = torch.cat([train_X[..., m, :].unsqueeze(dim=-3) for m in masks], dim=-3)
+    train_Y_cv = torch.cat(
+        [train_Y[..., ~m, :].unsqueeze(dim=-3) for m in masks], dim=-3
+    )
+    test_Y_cv = torch.cat([train_Y[..., m, :].unsqueeze(dim=-3) for m in masks], dim=-3)
+    if train_Yvar is None:
+        train_Yvar_cv = None
+        test_Yvar_cv = None
+    else:
+        train_Yvar_cv = torch.cat(
+            [train_Yvar[..., ~m, :].unsqueeze(dim=-3) for m in masks], dim=-3
+        )
+        test_Yvar_cv = torch.cat(
+            [train_Yvar[..., m, :].unsqueeze(dim=-3) for m in masks], dim=-3
+        )
     return CVFolds(
-        train_x=train_x_cv, train_y=train_y_cv, test_x=test_x_cv, test_y=test_y_cv
+        train_X=train_X_cv,
+        test_X=test_X_cv,
+        train_Y=train_Y_cv,
+        test_Y=test_Y_cv,
+        train_Yvar=train_Yvar_cv,
+        test_Yvar=test_Yvar_cv,
     )
 
 
 def batch_cross_validation(
-    model_cls: Type[ExactGP],
-    likelihood_cls: Type[GaussianLikelihood],
+    model_cls: Type[GPyTorchModel],
+    mll_cls: Type[MarginalLogLikelihood],
     cv_folds: CVFolds,
     fit_args: Optional[Dict[str, Any]] = None,
+    observation_noise: bool = False,
 ) -> CVResults:
     r"""Perform cross validation by using gpytorch batch mode
 
     Args:
-        model_cls: An ExactGP class. Must be able to work both in non-batch and
-            in batch mode, and take a "batch_shape" kwarg in its constructor
-        likelihood_cls: A GaussianLikelihood class. Must be able to work both in
-            non-batch and in batch mode, and take a "batch_shape" kwarg in its
-            constructor
-        cv_folds: A CVFolds tuple
+        model_cls: A GPyTorchModel class. This class must initialize the likelihood
+            internally. Note: Multi-task GPs are not currently supported.
+        mll_cls: A MarginalLogLikelihood class.
+        cv_folds: A CVFolds tuple.
         fit_args: Arguments passed along to fit_gpytorch_model
 
     Returns:
         A CVResults tuple with the following fields
 
-        - posterior: A batched torch Distribution, each batch representing
-        the posteriors of the model on the assocoated test point(s).
-        - observations: A `n` or `n x t` tensor of test observations.
+        - model: GPyTorchModel for batched cross validation
+        - posterior: GPyTorchPosterior where the mean has shape `n x 1 x o` or
+          `batch_shape x n x 1 x o`
+        - observed_Y: A `n x 1 x o` or `batch_shape x n x 1 x o` tensor of observations.
+        - observed_Yvar: A `n x 1 x o` or `batch_shape x n x 1 x o` tensor of observed
+          measurement noise.
 
     WARNING: This function is currently very memory inefficient, use it only
         for problems of small size.
     """
-    num_folds = cv_folds.train_x.shape[0]
-    likelihood_cv = likelihood_cls(batch_shape=torch.Size([num_folds]))
-    model_cv = model_cls(
-        cv_folds.train_x,
-        cv_folds.train_y,
-        likelihood_cv,
-        batch_shape=torch.Size([num_folds]),
-    )
-    mll_cv = ExactMarginalLogLikelihood(likelihood_cv, model_cv)
-    mll_cv.to(cv_folds.train_x)
+    fit_args = fit_args or {}
+    kwargs = {
+        "train_X": cv_folds.train_X,
+        "train_Y": cv_folds.train_Y,
+        "train_Yvar": cv_folds.train_Yvar,
+    }
+    model_cv = model_cls(**_filter_kwargs(model_cls, **kwargs))
+    mll_cv = mll_cls(model_cv.likelihood, model_cv)
+    mll_cv.to(cv_folds.train_X)
     mll_cv = fit_gpytorch_model(mll_cv, **fit_args)
 
     # Evaluate on the hold-out set in batch mode
     with torch.no_grad():
-        posterior = model_cv.posterior(cv_folds.test_x)
+        posterior = model_cv.posterior(
+            cv_folds.test_X, observation_noise=observation_noise
+        )
 
-    return CVResults(posterior=posterior, observed=cv_folds.test_y)
+    return CVResults(
+        model=model_cv,
+        posterior=posterior,
+        observed_Y=cv_folds.test_Y,
+        observed_Yvar=cv_folds.test_Yvar,
+    )
--- test/test_cross_validation.py
+++ test/test_cross_validation.py
@@ -5,141 +5,65 @@ import unittest
 
 import torch
 from botorch.cross_validation import batch_cross_validation, gen_loo_cv_folds
-from botorch.models.gpytorch import GPyTorchModel
-from gpytorch.distributions import MultitaskMultivariateNormal, MultivariateNormal
-from gpytorch.kernels import MultitaskKernel, RBFKernel, ScaleKernel
-from gpytorch.likelihoods import (
-    GaussianLikelihood,
-    MultitaskGaussianLikelihoodKronecker,
-)
-from gpytorch.means import ConstantMean, MultitaskMean
-from gpytorch.models import ExactGP
-from gpytorch.priors import NormalPrior, SmoothedBoxPrior
+from botorch.models.gp_regression import FixedNoiseGP, SingleTaskGP
+from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
 
 
-class CVExactGPModel(ExactGP, GPyTorchModel):
-    def __init__(self, train_inputs, train_targets, likelihood, batch_shape=None):
-        super().__init__(train_inputs, train_targets, likelihood)
-        if batch_shape is None:
-            batch_shape = torch.Size([1])
-        self.mean_module = ConstantMean(
-            batch_shape=batch_shape, prior=SmoothedBoxPrior(-10, 10)
-        )
-        self.covar_module = ScaleKernel(
-            RBFKernel(
-                batch_shape=batch_shape,
-                lengthscale_prior=NormalPrior(
-                    loc=torch.zeros(batch_shape + torch.Size([1, 1])),
-                    scale=torch.ones(batch_shape + torch.Size([1, 1])),
-                ),
-            ),
-            batch_shape=batch_shape,
-            outputscale_prior=SmoothedBoxPrior(-2, 2),
-        )
+def _get_random_data(batch_shape, num_outputs, n=10, **tkwargs):
+    train_x = torch.linspace(0, 0.95, n, **tkwargs).unsqueeze(-1) + 0.05 * torch.rand(
+        n, 1, **tkwargs
+    ).repeat(batch_shape + torch.Size([1, 1]))
+    train_y = torch.sin(train_x * (2 * math.pi)) + 0.2 * torch.randn(
+        n, num_outputs, **tkwargs
+    ).repeat(batch_shape + torch.Size([1, 1]))
 
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return MultivariateNormal(mean_x, covar_x)
-
-
-class CVGaussianLikelihood(GaussianLikelihood):
-    def __init__(self, batch_shape=None):
-        if batch_shape is None:
-            batch_shape = torch.Size([1])
-        super().__init__(
-            noise_prior=NormalPrior(
-                loc=torch.zeros(batch_shape), scale=torch.ones(batch_shape)
-            ),
-            batch_shape=batch_shape,
-        )
-
-
-class CVMultitaskExactGPModel(ExactGP, GPyTorchModel):
-    def __init__(self, train_inputs, train_targets, likelihood, batch_shape=None):
-        super().__init__(train_inputs, train_targets, likelihood)
-        if batch_shape is None:
-            batch_shape = torch.Size([1])
-        self.mean_module = MultitaskMean(
-            ConstantMean(batch_shape=batch_shape, prior=SmoothedBoxPrior(-10, 10)),
-            num_tasks=2,
-        )
-        self.covar_module = MultitaskKernel(
-            RBFKernel(
-                batch_shape=batch_shape,
-                lengthscale_prior=NormalPrior(
-                    loc=torch.zeros(batch_shape + torch.Size([1, 1])),
-                    scale=torch.ones(batch_shape + torch.Size([1, 1])),
-                ),
-            ),
-            num_tasks=2,
-            rank=1,
-        )
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return MultitaskMultivariateNormal(mean_x, covar_x)
-
-
-class CVMultitaskGaussianLikelihood(MultitaskGaussianLikelihoodKronecker):
-    def __init__(self, batch_shape=None):
-        if batch_shape is None:
-            batch_shape = torch.Size([1])
-        super().__init__(
-            noise_prior=NormalPrior(
-                loc=torch.zeros(batch_shape), scale=torch.ones(batch_shape)
-            ),
-            num_tasks=2,
-            batch_shape=batch_shape,
-        )
+    if num_outputs == 1:
+        train_y = train_y.squeeze(-1)
+    return train_x, train_y
 
 
 class TestFitBatchCrossValidation(unittest.TestCase):
     def test_single_task_batch_cv(self, cuda=False):
-        device = torch.device("cuda") if cuda else torch.device("cpu")
-        train_x = torch.linspace(0, 1, 5, device=device).view(-1, 1)
-        train_y = torch.sin(train_x * (2 * math.pi)).view(-1)
-        train_y += torch.randn_like(train_y).mul_(0.05)
-        cv_folds = gen_loo_cv_folds(train_x, train_y)
-        cv_results = batch_cross_validation(
-            model_cls=CVExactGPModel,
-            likelihood_cls=CVGaussianLikelihood,
-            cv_folds=cv_folds,
-            fit_args={"options": {"maxiter": 1}},
-        )
-        # compute MSE
-        ((cv_results.observed - cv_results.posterior.mean) ** 2).mean()
-        self.assertTrue(cv_results.posterior.mean.shape == torch.Size([5, 1, 1]))
+        n = 10
+        for batch_shape in (torch.Size([]), torch.Size([2])):
+            for num_outputs in (1, 2):
+                for double in (False, True):
+                    tkwargs = {
+                        "device": torch.device("cuda") if cuda else torch.device("cpu"),
+                        "dtype": torch.double if double else torch.float,
+                    }
+                    train_X, train_Y = _get_random_data(
+                        batch_shape=batch_shape, num_outputs=num_outputs, n=n, **tkwargs
+                    )
+                    train_Yvar = torch.full_like(train_Y, 0.01)
+                    noiseless_cv_folds = gen_loo_cv_folds(
+                        train_X=train_X, train_Y=train_Y
+                    )
+                    # Test SingleTaskGP
+                    cv_results = batch_cross_validation(
+                        model_cls=SingleTaskGP,
+                        mll_cls=ExactMarginalLogLikelihood,
+                        cv_folds=noiseless_cv_folds,
+                        fit_args={"options": {"maxiter": 1}},
+                    )
+                    expected_shape = batch_shape + torch.Size([n, 1, num_outputs])
+                    self.assertEqual(cv_results.posterior.mean.shape, expected_shape)
+                    self.assertEqual(cv_results.observed_Y.shape, expected_shape)
+
+                    # Test FixedNoiseGP
+                    noisy_cv_folds = gen_loo_cv_folds(
+                        train_X=train_X, train_Y=train_Y, train_Yvar=train_Yvar
+                    )
+                    cv_results = batch_cross_validation(
+                        model_cls=FixedNoiseGP,
+                        mll_cls=ExactMarginalLogLikelihood,
+                        cv_folds=noisy_cv_folds,
+                        fit_args={"options": {"maxiter": 1}},
+                    )
+                    self.assertEqual(cv_results.posterior.mean.shape, expected_shape)
+                    self.assertEqual(cv_results.observed_Y.shape, expected_shape)
+                    self.assertEqual(cv_results.observed_Y.shape, expected_shape)
 
     def test_single_task_batch_cv_cuda(self):
         if torch.cuda.is_available():
             self.test_single_task_batch_cv(cuda=True)
-
-    def test_multi_task_batch_cv(self, cuda=False):
-        device = torch.device("cuda") if cuda else torch.device("cpu")
-        train_x = torch.linspace(0, 1, 5, device=device).view(-1, 1)
-        train_y = torch.cat(
-            [torch.sin(train_x * (2 * math.pi)), torch.cos(train_x * (2 * math.pi))], -1
-        )
-        train_y += torch.randn_like(train_y).mul_(0.05)
-        cv_folds = gen_loo_cv_folds(train_x, train_y)
-        cv_results = batch_cross_validation(
-            model_cls=CVMultitaskExactGPModel,
-            likelihood_cls=CVMultitaskGaussianLikelihood,
-            cv_folds=cv_folds,
-            fit_args={"options": {"maxiter": 1}},
-        )
-        # compute MSE
-        ((cv_results.observed - cv_results.posterior.mean) ** 2).sum(-1).mean()
-        self.assertTrue(cv_results.posterior.mean.shape == torch.Size([5, 1, 2]))
-        # compute predicton errors
-        (cv_results.posterior.mean - cv_folds.test_y).detach().squeeze(1)
-        # compute predictive sems
-        torch.diagonal(
-            cv_results.posterior.mvn.covariance_matrix, dim1=-2, dim2=-1
-        ).sqrt().detach()
-
-    def test_multi_task_batch_cv_cuda(self):
-        if torch.cuda.is_available():
-            self.test_multi_task_batch_cv(cuda=True)
