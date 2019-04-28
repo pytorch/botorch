78b1b9e518c0ff3b74dcfe1f4ab3f68b89247dfa
Max Balandat <balandat@fb.com>
2019-04-28 13:05:31 -0700

    Full test coverage (#112)
    Summary:
    There it is
    
    <img width="533" alt="coverage" src="https://user-images.githubusercontent.com/1605878/56858779-046dd200-6935-11e9-8630-6ed42ef8a265.png">
    Pull Request resolved: https://github.com/pytorch/botorch/pull/112
    
    Reviewed By: eytan
    
    Differential Revision: D15116068
    
    Pulled By: Balandat
    
    fbshipit-source-id: 9e540776c4dc227fe580efdaf3b16e843e6cfb80

--- test/models/test_multitask.py
+++ test/models/test_multitask.py
@@ -5,11 +5,11 @@ import unittest
 
 import torch
 from botorch import fit_gpytorch_model
-from botorch.models.multitask import MultiTaskGP
+from botorch.models.multitask import FixedNoiseMultiTaskGP, MultiTaskGP
 from botorch.posteriors import GPyTorchPosterior
 from gpytorch.distributions import MultitaskMultivariateNormal, MultivariateNormal
 from gpytorch.kernels import IndexKernel, MaternKernel, ScaleKernel
-from gpytorch.likelihoods import GaussianLikelihood
+from gpytorch.likelihoods import FixedNoiseGaussianLikelihood, GaussianLikelihood
 from gpytorch.means import ConstantMean
 from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
 from gpytorch.priors import GammaPrior
@@ -30,14 +30,30 @@ def _get_random_mt_data(**tkwargs):
 
 
 def _get_model(**tkwargs):
-    train_X, train_Y, = _get_random_mt_data(**tkwargs)
+    train_X, train_Y = _get_random_mt_data(**tkwargs)
     model = MultiTaskGP(train_X, train_Y, task_feature=1)
     return model.to(**tkwargs)
 
 
 def _get_model_single_output(**tkwargs):
-    train_X, train_Y, = _get_random_mt_data(**tkwargs)
+    train_X, train_Y = _get_random_mt_data(**tkwargs)
     model = MultiTaskGP(train_X, train_Y, task_feature=1, output_tasks=[1])
+    return model.to(**tkwargs)
+
+
+def _get_fixed_noise_model(**tkwargs):
+    train_X, train_Y = _get_random_mt_data(**tkwargs)
+    train_Yvar = torch.full_like(train_Y, 0.05)
+    model = FixedNoiseMultiTaskGP(train_X, train_Y, train_Yvar, task_feature=1)
+    return model.to(**tkwargs)
+
+
+def _get_fixed_noise_model_single_output(**tkwargs):
+    train_X, train_Y = _get_random_mt_data(**tkwargs)
+    train_Yvar = torch.full_like(train_Y, 0.05)
+    model = FixedNoiseMultiTaskGP(
+        train_X, train_Y, train_Yvar, task_feature=1, output_tasks=[1]
+    )
     return model.to(**tkwargs)
 
 
@@ -74,6 +90,13 @@ class TestMultiTaskGP(unittest.TestCase):
             self.assertEqual(posterior_f.mean.shape, torch.Size([2, 2]))
             self.assertEqual(posterior_f.variance.shape, torch.Size([2, 2]))
 
+            # test posterior w/ observation noise
+            posterior_o = model.posterior(test_x, observation_noise=True)
+            self.assertIsInstance(posterior_o, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            self.assertEqual(posterior_f.mean.shape, torch.Size([2, 2]))
+            self.assertEqual(posterior_f.variance.shape, torch.Size([2, 2]))
+
             # test posterior w/ single output index
             posterior_f = model.posterior(test_x, output_indices=[0])
             self.assertIsInstance(posterior_f, GPyTorchPosterior)
@@ -90,6 +113,19 @@ class TestMultiTaskGP(unittest.TestCase):
             posterior_f = model.posterior(test_x)
             self.assertIsInstance(posterior_f, GPyTorchPosterior)
             self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+
+            # test that unsupported batch shape MTGPs throw correct error
+            with self.assertRaises(ValueError):
+                MultiTaskGP(torch.rand(2, 2, 2), torch.rand(2, 1), 0)
+
+            # test that bad feature index throws correct error
+            train_X, train_Y = _get_random_mt_data(**tkwargs)
+            with self.assertRaises(ValueError):
+                MultiTaskGP(train_X, train_Y, 2)
+
+            # test that bad output task throws correct error
+            with self.assertRaises(RuntimeError):
+                MultiTaskGP(train_X, train_Y, 0, output_tasks=[2])
 
     def test_MultiTaskGP_cuda(self):
         if torch.cuda.is_available():
@@ -134,3 +170,116 @@ class TestMultiTaskGP(unittest.TestCase):
     def test_MultiTaskGP_single_output_cuda(self):
         if torch.cuda.is_available():
             self.test_MultiTaskGP_single_output(cuda=True)
+
+
+class TestFixedNoiseMultiTaskGP(unittest.TestCase):
+    def test_FixedNoiseMultiTaskGP(self, cuda=False):
+        for double in (False, True):
+            tkwargs = {
+                "device": torch.device("cuda") if cuda else torch.device("cpu"),
+                "dtype": torch.double if double else torch.float,
+            }
+            model = _get_fixed_noise_model(**tkwargs)
+            self.assertIsInstance(model, FixedNoiseMultiTaskGP)
+            self.assertIsInstance(model.likelihood, FixedNoiseGaussianLikelihood)
+            self.assertIsInstance(model.mean_module, ConstantMean)
+            self.assertIsInstance(model.covar_module, ScaleKernel)
+            matern_kernel = model.covar_module.base_kernel
+            self.assertIsInstance(matern_kernel, MaternKernel)
+            self.assertIsInstance(matern_kernel.lengthscale_prior, GammaPrior)
+            self.assertIsInstance(model.task_covar_module, IndexKernel)
+            self.assertEqual(model._rank, 2)
+            self.assertEqual(
+                model.task_covar_module.covar_factor.shape[-1], model._rank
+            )
+
+            # test model fitting
+            mll = ExactMarginalLogLikelihood(model.likelihood, model)
+            mll = fit_gpytorch_model(mll, options={"maxiter": 1})
+
+            # test posterior
+            test_x = torch.rand(2, 1, **tkwargs)
+            posterior_f = model.posterior(test_x)
+            self.assertIsInstance(posterior_f, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            self.assertEqual(posterior_f.mean.shape, torch.Size([2, 2]))
+            self.assertEqual(posterior_f.variance.shape, torch.Size([2, 2]))
+
+            # TODO: test posterior w/ observation noise
+
+            # test posterior w/ single output index
+            posterior_f = model.posterior(test_x, output_indices=[0])
+            self.assertIsInstance(posterior_f, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultivariateNormal)
+            self.assertEqual(posterior_f.mean.shape, torch.Size([2, 1]))
+            self.assertEqual(posterior_f.variance.shape, torch.Size([2, 1]))
+
+            # test posterior w/ bad output index
+            with self.assertRaises(ValueError):
+                model.posterior(test_x, output_indices=[2])
+
+            # test posterior (batch eval)
+            test_x = torch.rand(3, 2, 1, **tkwargs)
+            posterior_f = model.posterior(test_x)
+            self.assertIsInstance(posterior_f, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+
+            # test that unsupported batch shape MTGPs throw correct error
+            with self.assertRaises(ValueError):
+                FixedNoiseMultiTaskGP(
+                    torch.rand(2, 2, 2), torch.rand(2, 1), torch.rand(2, 1), 0
+                )
+
+            # test that bad feature index throws correct error
+            train_X, train_Y = _get_random_mt_data(**tkwargs)
+            train_Yvar = torch.full_like(train_Y, 0.05)
+            with self.assertRaises(ValueError):
+                FixedNoiseMultiTaskGP(train_X, train_Y, train_Yvar, 2)
+
+            # test that bad output task throws correct error
+            with self.assertRaises(RuntimeError):
+                FixedNoiseMultiTaskGP(train_X, train_Y, train_Yvar, 0, output_tasks=[2])
+
+    def test_FixedNoiseMultiTaskGP_cuda(self):
+        if torch.cuda.is_available():
+            self.test_FixedNoiseMultiTaskGP(cuda=True)
+
+    def test_FixedNoiseMultiTaskGP_single_output(self, cuda=False):
+        for double in (False, True):
+            tkwargs = {
+                "device": torch.device("cuda") if cuda else torch.device("cpu"),
+                "dtype": torch.double if double else torch.float,
+            }
+            model = _get_fixed_noise_model_single_output(**tkwargs)
+            self.assertIsInstance(model, FixedNoiseMultiTaskGP)
+            self.assertIsInstance(model.likelihood, FixedNoiseGaussianLikelihood)
+            self.assertIsInstance(model.mean_module, ConstantMean)
+            self.assertIsInstance(model.covar_module, ScaleKernel)
+            matern_kernel = model.covar_module.base_kernel
+            self.assertIsInstance(matern_kernel, MaternKernel)
+            self.assertIsInstance(matern_kernel.lengthscale_prior, GammaPrior)
+            self.assertIsInstance(model.task_covar_module, IndexKernel)
+            self.assertEqual(model._rank, 2)
+            self.assertEqual(
+                model.task_covar_module.covar_factor.shape[-1], model._rank
+            )
+
+            # test model fitting
+            mll = ExactMarginalLogLikelihood(model.likelihood, model)
+            mll = fit_gpytorch_model(mll, options={"maxiter": 1})
+
+            # test posterior
+            test_x = torch.rand(2, 1, **tkwargs)
+            posterior_f = model.posterior(test_x)
+            self.assertIsInstance(posterior_f, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultivariateNormal)
+
+            # test posterior (batch eval)
+            test_x = torch.rand(3, 2, 1, **tkwargs)
+            posterior_f = model.posterior(test_x)
+            self.assertIsInstance(posterior_f, GPyTorchPosterior)
+            self.assertIsInstance(posterior_f.mvn, MultivariateNormal)
+
+    def test_FixedNoiseMultiTaskGP_single_output_cuda(self):
+        if torch.cuda.is_available():
+            self.test_FixedNoiseMultiTaskGP_single_output(cuda=True)
