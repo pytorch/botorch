Fix overflow and bug in initialize_q_batch.
Summary: The heuristic was not selecting points properly because the parenthesis were in the wrong place and further the exponential could result in overflow with extreme values of eta*Z.  This diff fixes both issues and adds a corresponding unit test.

Reviewed By: bkarrer

Differential Revision: D15577608

fbshipit-source-id: 964487b2af4dafc8622e3331482d3336017bff03

botorch/optim/initializers.py
@@ -58,8 +58,12 @@ def initialize_q_batch(X: Tensor, Y: Tensor, n: int, eta: float = 1.0) -> Tensor
         return X[torch.randperm(n=n_samples, device=X.device)][:n]
 
     max_val, max_idx = torch.max(Y, dim=0)
-    Z = Y - Y.mean() / Ystd
-    weights = torch.exp(eta * Z)
+    Z = (Y - Y.mean()) / Ystd
+    etaZ = eta * Z
+    weights = torch.exp(etaZ)
+    while torch.isinf(weights).any():
+        etaZ *= 0.5
+        weights = torch.exp(etaZ)
     idcs = torch.multinomial(weights, n)
     # make sure we get the maximum
     if max_idx not in idcs: