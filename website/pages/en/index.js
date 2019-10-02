c6b60048167721304896ea0794afd24516d652a0
Max Balandat <balandat@fb.com>
2019-10-01 18:22:45 -0700

    Fix getting started example on website
    This makes sure the example uses the convention of explicit output dimensions.
    
    Test Plan:
    Tested locally in nb

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -115,11 +115,13 @@ class Index extends React.Component {
 import torch
 from botorch.models import SingleTaskGP
 from botorch.fit import fit_gpytorch_model
+from botorch.utils import standardize
 from gpytorch.mlls import ExactMarginalLogLikelihood
 
 train_X = torch.rand(10, 2)
-Y = 1 - torch.norm(train_X - 0.5, dim=-1) + 0.1 * torch.rand(10)
-train_Y = (Y - Y.mean()) / Y.std()
+Y = 1 - torch.norm(train_X - 0.5, dim=-1, keepdim=True)
+Y = Y + 0.1 * torch.randn_like(Y)  # add some noise
+train_Y = standardize(Y)
 
 gp = SingleTaskGP(train_X, train_Y)
 mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
