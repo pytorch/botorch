f053d57fa3c494fe4e9eac9fee073b31637546dc
Max Balandat <balandat@fb.com>
2019-11-01 11:25:20 -0700

    New license header style (#309)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/309
    
    New license header style.
    
    Reviewed By: zertosh
    
    Differential Revision: D18272981
    
    fbshipit-source-id: 3993e08cdce191cf00bc5437bae4805f3c4fd6cf

--- botorch/optim/__init__.py
+++ botorch/optim/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from .initializers import initialize_q_batch, initialize_q_batch_nonneg
 from .numpy_converter import module_to_array, set_params_with_array
--- botorch/utils/__init__.py
+++ botorch/utils/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from .constraints import get_outcome_constraint_transforms
 from .objective import apply_constraints, get_objective_weights_transform
