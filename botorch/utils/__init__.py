5b4e3a9995d6f404c9ab30c37dcbfc620026fdb5
Sam Daulton <sdaulton@fb.com>
2020-09-17 20:26:15 -0700

    Differentiable approximate rounding for integers
    Summary: see title
    
    Differential Revision: D23455436
    
    fbshipit-source-id: d8e1835b2021a99fa25f0e71ec5340af2ef60255

--- botorch/utils/__init__.py
+++ botorch/utils/__init__.py
@@ -7,6 +7,7 @@
 from botorch.utils.constraints import get_outcome_constraint_transforms
 from botorch.utils.feasible_volume import estimate_feasible_volume
 from botorch.utils.objective import apply_constraints, get_objective_weights_transform
+from botorch.utils.rounding import approximate_round
 from botorch.utils.sampling import (
     batched_multinomial,
     construct_base_samples,
@@ -24,16 +25,17 @@ from botorch.utils.transforms import (
 
 __all__ = [
     "apply_constraints",
+    "approximate_round",
     "batched_multinomial",
     "construct_base_samples",
     "construct_base_samples_from_posterior",
     "draw_sobol_normal_samples",
     "draw_sobol_samples",
+    "estimate_feasible_volume",
     "get_objective_weights_transform",
     "get_outcome_constraint_transforms",
     "manual_seed",
     "squeeze_last_dim",
     "standardize",
     "t_batch_mode_transform",
-    "estimate_feasible_volume",
 ]
