dc9f7771739b9cbd662a1e6780b2694f2cf333ab
Sam Daulton <sdaulton@fb.com>
2021-01-11 10:34:52 -0800

    remove duplicates in is_non_dominated (#657)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/657
    
    see title. Before there could be duplicates in the pareto set.
    
    Reviewed By: Balandat
    
    Differential Revision: D25753915
    
    fbshipit-source-id: cd164718945ca91ad22208206d9568eb2e40b36e

--- botorch/utils/multi_objective/pareto.py
+++ botorch/utils/multi_objective/pareto.py
@@ -6,23 +6,33 @@
 
 from __future__ import annotations
 
+import torch
 from torch import Tensor
 
 
-def is_non_dominated(Y: Tensor) -> Tensor:
+def is_non_dominated(Y: Tensor, deduplicate: bool = True) -> Tensor:
     r"""Computes the non-dominated front.
 
     Note: this assumes maximization.
 
     Args:
-        Y: a `(batch_shape) x n x m`-dim tensor of outcomes.
+        Y: A `(batch_shape) x n x m`-dim tensor of outcomes.
+        deduplicate: A boolean indicating whether to only return
+            unique points on the pareto frontier.
 
     Returns:
         A `(batch_shape) x n`-dim boolean tensor indicating whether
         each point is non-dominated.
     """
-    expanded_shape = Y.shape[:-2] + Y.shape[-2:-1] + Y.shape[-2:]
-    Y1 = Y.unsqueeze(-3).expand(expanded_shape)
-    Y2 = Y.unsqueeze(-2).expand(expanded_shape)
+    Y1 = Y.unsqueeze(-3)
+    Y2 = Y.unsqueeze(-2)
     dominates = (Y1 >= Y2).all(dim=-1) & (Y1 > Y2).any(dim=-1)
-    return ~(dominates.any(dim=-1))
+    nd_mask = ~(dominates.any(dim=-1))
+    if deduplicate:
+        # remove duplicates
+        # find index of first occurrence  of each unique element
+        indices = (Y1 == Y2).all(dim=-1).long().argmax(dim=-1)
+        keep = torch.zeros_like(nd_mask)
+        keep.scatter_(dim=-1, index=indices, value=1.0)
+        return nd_mask & keep
+    return nd_mask
