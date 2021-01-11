d560b2f6ba86d0f9a8ea77b70cf281c11f1364a1
Sam Daulton <sdaulton@fb.com>
2021-01-10 22:34:22 -0800

    remove duplicates in is_non_dominated (#657)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/657
    
    see title. Before there could be duplicates in the pareto set.
    
    Differential Revision: D25753915
    
    fbshipit-source-id: cfb51e798d8fb3c9f09038c01d6c88cc717cb9f3

--- test/utils/multi_objective/test_pareto.py
+++ test/utils/multi_objective/test_pareto.py
@@ -22,6 +22,7 @@ class TestPareto(BotorchTestCase):
                 [4.0, 5.0],
                 [5.0, 5.0],
                 [8.5, 3.5],
+                [8.5, 3.5],
                 [8.5, 3.0],
                 [9.0, 1.0],
             ]
@@ -35,6 +36,7 @@ class TestPareto(BotorchTestCase):
                 [4.0, 2.0, 3.0],
                 [2.0, 4.0, 1.0],
                 [3.0, 5.0, 1.0],
+                [2.0, 4.0, 2.0],
                 [2.0, 4.0, 2.0],
                 [1.0, 3.0, 4.0],
                 [1.0, 2.0, 4.0],
@@ -67,15 +69,39 @@ class TestPareto(BotorchTestCase):
             # test 2d
             nondom_Y = Y[is_non_dominated(Y)]
             self.assertTrue(torch.equal(expected_nondom_Y, nondom_Y))
+            # test deduplicate=False
+            expected_nondom_Y_no_dedup = torch.cat(
+                [expected_nondom_Y, expected_nondom_Y[-1:]], dim=0
+            )
+            nondom_Y = Y[is_non_dominated(Y, deduplicate=False)]
+            self.assertTrue(torch.equal(expected_nondom_Y_no_dedup, nondom_Y))
+
             # test batch
             batch_Y = torch.stack([Y, Yb], dim=0)
             nondom_mask = is_non_dominated(batch_Y)
             self.assertTrue(torch.equal(batch_Y[0][nondom_mask[0]], expected_nondom_Y))
             self.assertTrue(torch.equal(batch_Y[1][nondom_mask[1]], expected_nondom_Yb))
+            # test deduplicate=False
+            expected_nondom_Yb_no_dedup = torch.cat(
+                [expected_nondom_Yb[:-1], expected_nondom_Yb[-2:]], dim=0
+            )
+            nondom_mask = is_non_dominated(batch_Y, deduplicate=False)
+            self.assertTrue(
+                torch.equal(batch_Y[0][nondom_mask[0]], expected_nondom_Y_no_dedup)
+            )
+            self.assertTrue(
+                torch.equal(batch_Y[1][nondom_mask[1]], expected_nondom_Yb_no_dedup)
+            )
 
             # test 3d
             nondom_Y3 = Y3[is_non_dominated(Y3)]
             self.assertTrue(torch.equal(expected_nondom_Y3, nondom_Y3))
+            # test deduplicate=False
+            expected_nondom_Y3_no_dedup = torch.cat(
+                [expected_nondom_Y3[:3], expected_nondom_Y3[2:]], dim=0
+            )
+            nondom_Y3 = Y3[is_non_dominated(Y3, deduplicate=False)]
+            self.assertTrue(torch.equal(expected_nondom_Y3_no_dedup, nondom_Y3))
             # test batch
             batch_Y3 = torch.stack([Y3, Y3b], dim=0)
             nondom_mask3 = is_non_dominated(batch_Y3)
@@ -84,4 +110,15 @@ class TestPareto(BotorchTestCase):
             )
             self.assertTrue(
                 torch.equal(batch_Y3[1][nondom_mask3[1]], expected_nondom_Y3b)
+            )
+            # test deduplicate=False
+            nondom_mask3 = is_non_dominated(batch_Y3, deduplicate=False)
+            self.assertTrue(
+                torch.equal(batch_Y3[0][nondom_mask3[0]], expected_nondom_Y3_no_dedup)
+            )
+            expected_nondom_Y3b_no_dedup = torch.cat(
+                [expected_nondom_Y3b[:2], expected_nondom_Y3b[1:]], dim=0
+            )
+            self.assertTrue(
+                torch.equal(batch_Y3[1][nondom_mask3[1]], expected_nondom_Y3b_no_dedup)
             )
