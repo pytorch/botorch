14ad9a3e0ed50f372843ad890788d2c2b103a1be
Max Balandat <balandat@fb.com>
2021-04-19 16:21:40 -0700

    Categorical Kernel (#771)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/771
    
    Implements a categorical kernel.
    
    Reviewed By: dme65
    
    Differential Revision: D27194283
    
    fbshipit-source-id: 63351494f548dc83b14359ede90ee214d5c29783

--- botorch/models/kernels/__init__.py
+++ botorch/models/kernels/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from botorch.models.kernels.categorical import CategoricalKernel
 from botorch.models.kernels.downsampling import DownsamplingKernel
 from botorch.models.kernels.exponential_decay import ExponentialDecayKernel
 from botorch.models.kernels.linear_truncated_fidelity import (
@@ -12,6 +13,7 @@ from botorch.models.kernels.linear_truncated_fidelity import (
 
 
 __all__ = [
+    "CategoricalKernel",
     "DownsamplingKernel",
     "ExponentialDecayKernel",
     "LinearTruncatedFidelityKernel",
