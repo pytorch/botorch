496c0697ac7c3210505c2a578183cbe56f8ec201
Sam Daulton <sdaulton@fb.com>
2021-06-18 10:37:35 -0700

    add heuristic for reference point selection (#830)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/830
    
    Pull Request resolved: https://github.com/facebookexternal/botorch_fb/pull/9
    
    This heuristic is useful for setting the reference point to for EHVI to identify the entire pareto frontier.
    
    Reviewed By: Balandat
    
    Differential Revision: D28163747
    
    fbshipit-source-id: b26c49772db7f5670e56d50777b8de987a17ec58

--- botorch/utils/multi_objective/__init__.py
+++ botorch/utils/multi_objective/__init__.py
@@ -4,13 +4,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from botorch.utils.multi_objective.hypervolume import Hypervolume
+from botorch.utils.multi_objective.hypervolume import Hypervolume, infer_reference_point
 from botorch.utils.multi_objective.pareto import is_non_dominated
 from botorch.utils.multi_objective.scalarization import get_chebyshev_scalarization
 
 
 __all__ = [
     "get_chebyshev_scalarization",
+    "infer_reference_point",
     "is_non_dominated",
     "Hypervolume",
 ]
