cec47a38b69103d36de5df003a4cd4ab5f32c268
Ryan Roussel <roussel@ucla.edu>
2021-08-26 15:09:48 -0700

    Adding proximal acquisition function wrapper (#919)
    Summary:
    ## Motivation
    
    The goal of this acquisition function is to bias a GP optimization towards smooth optimization through the input domain. The proximal AF multiplies the base acquisition function by a squared exponential with a user defined lengthscale, centered at the most recently observed training point (assumed to be model.train_inputs[-1]). If the associated lengthscale is short the algorithm makes small jumps in input space, if it is long it is not strongly biased. This method differs from simply restricting the max travel size in input space by allowing large travel distances if the predicted value is large enough. See https://journals.aps.org/prab/abstract/10.1103/PhysRevAccelBeams.24.062801 for discussion and analysis.
    
    This becomes relevant when using Bayesian optimization techniques on optimizing physical systems, where there is a cost associated with changing input parameters.
    
    ### Have you read the [Contributing Guidelines on pull requests](https://github.com/pytorch/botorch/blob/main/CONTRIBUTING.md#pull-requests)?
    
    Yes, I've tried my best to satisfy all requirements although there are possibly errors (first time contributing to a major project like this).
    
    Pull Request resolved: https://github.com/pytorch/botorch/pull/919
    
    Test Plan:
    Test script test_proximal.py has been added to test/acquisition. I can also provide numerical proof that this works with a simple script but I was unsure where to include. Result is show below
    ![figure_1](https://user-images.githubusercontent.com/24279776/130523338-05f00937-0c0e-4847-800b-96a02c92c411.png)
    
    Please comment if I need to change anything, thanks!
    
    ## Related PRs
    None
    
    Reviewed By: qingfeng10
    
    Differential Revision: D30575012
    
    Pulled By: Balandat
    
    fbshipit-source-id: ff462f3725a8c88bc60f3e6453a35df0e2435824

---
+++ botorch/acquisition/proximal.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+r"""
+A wrapper around AcquisitionFunctions to add proximal weighting of the
+acquisition function.
+"""
+
+from __future__ import annotations
+
+import torch
+from botorch.acquisition import AcquisitionFunction
+from botorch.exceptions.errors import UnsupportedError
+from botorch.utils import t_batch_mode_transform
+from torch import Tensor
+from torch.nn import Module
+
+
+class ProximalAcquisitionFunction(AcquisitionFunction):
+    """A wrapper around AcquisitionFunctions to add proximal weighting of the
+    acquisition function. Acquisition function is weighted via a squared exponential
+    centered at the last training point, with varying lengthscales corresponding to
+    `proximal_weights`. Can only be used with acquisition functions based on single
+    batch models.
+
+    Small values of `proximal_weights` corresponds to strong biasing towards recently
+    observed points, which smoothes optimization with a small potential decrese in
+    convergence rate.
+
+    Example:
+        >>> model = SingleTaskGP(train_X, train_Y)
+        >>> EI = ExpectedImprovement(model, best_f=0.0)
+        >>> proximal_weights = torch.ones(d)
+        >>> EI_proximal = ProximalAcquisitionFunction(EI, proximal_weights)
+        >>> eip = EI_proximal(test_X)
+    """
+
+    def __init__(
+        self,
+        acq_function: AcquisitionFunction,
+        proximal_weights: Tensor,
+    ) -> None:
+        r"""Derived Acquisition Function weighted by proximity to recently
+        observed point.
+
+        Args:
+            acq_function: The base acquisition function, operating on input tensors
+                of feature dimension `d`.
+            proximal_weights: A `d` dim tensor used to bias locality
+                along each axis.
+        """
+        Module.__init__(self)
+
+        self.acq_func = acq_function
+
+        self.register_buffer("proximal_weights", proximal_weights)
+
+        # check model for train_inputs and single batch
+        if not hasattr(self.acq_func.model, "train_inputs"):
+            raise UnsupportedError(
+                "Acquisition function model must have " "`train_inputs`."
+            )
+
+        if (
+            self.acq_func.model.batch_shape != torch.Size([])
+            and self.acq_func.model.train_inputs[0].shape[1] != 1
+        ):
+            raise UnsupportedError(
+                "Proximal acquisition function requires a single batch model"
+            )
+
+        # check to make sure that weights match the training data shape
+        if (
+            self.proximal_weights.shape[0]
+            != self.acq_func.model.train_inputs[0][-1].shape[-1]
+        ):
+            raise ValueError(
+                "`proximal_weights` must be a one dimensional tensor with "
+                "same feature dimension as model."
+            )
+
+    @t_batch_mode_transform(expected_q=1, assert_output_shape=False)
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate base acquisition function with proximal weighting.
+
+        Args:
+            X: Input tensor of feature dimension `d` .
+
+        Returns:
+            Base acquisition function evaluated on tensor `X` multiplied by proximal
+            weighting.
+        """
+        last_X = self.acq_func.model.train_inputs[0][-1].reshape(1, 1, -1)
+        diff = X - last_X
+
+        M = torch.linalg.norm(diff / self.proximal_weights, dim=-1) ** 2
+
+        proximal_acq_weight = torch.exp(-0.5 * M)
+        return self.acq_func(X) * proximal_acq_weight.flatten()
