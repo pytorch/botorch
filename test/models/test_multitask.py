98d08bdfb2a050ce599f955cdae97891caf75c2c
wjmaddox <wjm363@nyu.edu>
2021-12-06 10:47:33 -0800

    Use TransformedPosterior for subclasses of GPyTorchPosterior (#983)
    Summary:
    Resolves https://github.com/pytorch/botorch/issues/982 by returning a `TransformedPosterior` instead of a `GPyTorchPosterior` for objects of class `MultitaskGPPosterior`. This enables differentiability plus the exploitation of Matheron's rule for these types of transforms.
    
    ## Motivation
    
    Resolves https://github.com/pytorch/botorch/issues/982, can still look into what's going on with the actual error.
    
    ### Have you read the [Contributing Guidelines on pull requests](https://github.com/pytorch/botorch/blob/main/CONTRIBUTING.md#pull-requests)?
    
    Yes.
    
    Pull Request resolved: https://github.com/pytorch/botorch/pull/983
    
    Test Plan:
    Not entirely sure how to test this one out.
    
    ## Related PRs
    
    Might be a gpytorch related PR to fix the actual issue soon.
    
    Reviewed By: dme65
    
    Differential Revision: D32519742
    
    Pulled By: Balandat
    
    fbshipit-source-id: c25e19bae0df5b69c3531a40972388b9cae7f11c

--- test/models/test_multitask.py
+++ test/models/test_multitask.py
@@ -19,6 +19,7 @@ from botorch.models.multitask import (
 from botorch.models.transforms.input import Normalize
 from botorch.models.transforms.outcome import Standardize
 from botorch.posteriors import GPyTorchPosterior
+from botorch.posteriors.transformed import TransformedPosterior
 from botorch.utils.containers import TrainingData
 from botorch.utils.testing import BotorchTestCase
 from gpytorch.distributions import MultitaskMultivariateNormal, MultivariateNormal
@@ -630,8 +631,15 @@ class TestKroneckerMultiTaskGP(BotorchTestCase):
             # test posterior
             test_x = torch.rand(2, 2, **tkwargs)
             posterior_f = model.posterior(test_x)
-            self.assertIsInstance(posterior_f, GPyTorchPosterior)
-            self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            if not use_octf:
+                self.assertIsInstance(posterior_f, GPyTorchPosterior)
+                self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            else:
+                self.assertIsInstance(posterior_f, TransformedPosterior)
+                self.assertIsInstance(
+                    posterior_f._posterior.mvn, MultitaskMultivariateNormal
+                )
+
             self.assertEqual(posterior_f.mean.shape, torch.Size([2, 2]))
             self.assertEqual(posterior_f.variance.shape, torch.Size([2, 2]))
 
@@ -657,8 +665,14 @@ class TestKroneckerMultiTaskGP(BotorchTestCase):
             # test posterior (batch eval)
             test_x = torch.rand(3, 2, 2, **tkwargs)
             posterior_f = model.posterior(test_x)
-            self.assertIsInstance(posterior_f, GPyTorchPosterior)
-            self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            if not use_octf:
+                self.assertIsInstance(posterior_f, GPyTorchPosterior)
+                self.assertIsInstance(posterior_f.mvn, MultitaskMultivariateNormal)
+            else:
+                self.assertIsInstance(posterior_f, TransformedPosterior)
+                self.assertIsInstance(
+                    posterior_f._posterior.mvn, MultitaskMultivariateNormal
+                )
             self.assertEqual(posterior_f.mean.shape, torch.Size([3, 2, 2]))
             self.assertEqual(posterior_f.variance.shape, torch.Size([3, 2, 2]))
 
