ddf7c1a98b0e9c57f2e6f9aa2f029bf1d3e44888
Sam Daulton <sdaulton@fb.com>
2021-12-07 19:32:37 -0800

    support single output posteriors in sample_cached_cholesky (#995)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/995
    
    see title
    
    Reviewed By: Balandat
    
    Differential Revision: D32668106
    
    fbshipit-source-id: f94cefee72c061982c399adbdaa04dc596b4ce1a

--- test/acquisition/multi_objective/test_utils.py
+++ test/acquisition/multi_objective/test_utils.py
@@ -24,13 +24,11 @@ from botorch.exceptions.errors import BotorchError, UnsupportedError
 from botorch.exceptions.warnings import SamplingWarning
 from botorch.models.gp_regression import SingleTaskGP
 from botorch.models.model_list_gp_regression import ModelListGP
-from botorch.posteriors.gpytorch import GPyTorchPosterior
 from botorch.sampling.samplers import IIDNormalSampler
 from botorch.utils.testing import BotorchTestCase, MockModel, MockPosterior
 from gpytorch.distributions.multitask_multivariate_normal import (
     MultitaskMultivariateNormal,
 )
-from gpytorch.distributions.multivariate_normal import MultivariateNormal
 from gpytorch.lazy import lazify
 from gpytorch.lazy.block_diag_lazy_tensor import BlockDiagLazyTensor
 from gpytorch.utils.errors import NanError
@@ -220,109 +218,102 @@ class TestSampleCachedCholesky(BotorchTestCase):
     def test_sample_cached_cholesky(self):
         torch.manual_seed(0)
         tkwargs = {"device": self.device}
-        # test single output posterior
-        with self.assertRaises(NotImplementedError):
-            sample_cached_cholesky(
-                posterior=GPyTorchPosterior(
-                    MultivariateNormal(torch.zeros(2), torch.eye(2))
-                ),
-                baseline_L=None,
-                q=1,
-                base_samples=None,
-                sample_shape=None,
-            )
         for dtype in (torch.float, torch.double):
             tkwargs["dtype"] = dtype
             train_X = torch.rand(10, 2, **tkwargs)
             train_Y = torch.randn(10, 2, **tkwargs)
-            for use_model_list in (True, False):
-                if use_model_list:
-                    model = ModelListGP(
-                        SingleTaskGP(
+            for m in (1, 2):
+                model_list_values = (True, False) if m == 2 else (False,)
+                for use_model_list in model_list_values:
+                    if use_model_list:
+                        model = ModelListGP(
+                            SingleTaskGP(
+                                train_X,
+                                train_Y[..., :1],
+                            ),
+                            SingleTaskGP(
+                                train_X,
+                                train_Y[..., 1:],
+                            ),
+                        )
+                    else:
+                        model = SingleTaskGP(
                             train_X,
-                            train_Y[..., :1],
-                        ),
-                        SingleTaskGP(
-                            train_X,
-                            train_Y[..., 1:],
-                        ),
-                    )
-                else:
-                    model = SingleTaskGP(
-                        train_X,
-                        train_Y,
-                    )
-                sampler = IIDNormalSampler(3)
-                for q in (1, 3):
-                    # test batched baseline_L
-                    for train_batch_shape in (
-                        torch.Size([]),
-                        torch.Size([3]),
-                        torch.Size([3, 2]),
-                    ):
-                        # test batched test points
-                        for test_batch_shape in (
+                            train_Y[:, :m],
+                        )
+                    sampler = IIDNormalSampler(3)
+                    for q in (1, 3):
+                        # test batched baseline_L
+                        for train_batch_shape in (
                             torch.Size([]),
-                            torch.Size([4]),
-                            torch.Size([4, 2]),
+                            torch.Size([3]),
+                            torch.Size([3, 2]),
                         ):
+                            # test batched test points
+                            for test_batch_shape in (
+                                torch.Size([]),
+                                torch.Size([4]),
+                                torch.Size([4, 2]),
+                            ):
 
-                            if len(train_batch_shape) > 0:
-                                train_X_ex = train_X.unsqueeze(0).expand(
-                                    train_batch_shape + train_X.shape
+                                if len(train_batch_shape) > 0:
+                                    train_X_ex = train_X.unsqueeze(0).expand(
+                                        train_batch_shape + train_X.shape
+                                    )
+                                else:
+                                    train_X_ex = train_X
+                                if len(test_batch_shape) > 0:
+                                    test_X = train_X_ex.unsqueeze(0).expand(
+                                        test_batch_shape + train_X_ex.shape
+                                    )
+                                else:
+                                    test_X = train_X_ex
+                                with torch.no_grad():
+                                    base_posterior = model.posterior(
+                                        train_X_ex[..., :-q, :]
+                                    )
+                                    mvn = base_posterior.mvn
+                                    lazy_covar = mvn.lazy_covariance_matrix
+                                    if m == 2:
+                                        lazy_covar = lazy_covar.base_lazy_tensor
+                                    baseline_L = lazy_covar.root_decomposition()
+                                    baseline_L = baseline_L.root.evaluate()
+                                test_X = test_X.clone().requires_grad_(True)
+                                new_posterior = model.posterior(test_X)
+                                samples = sampler(new_posterior)
+                                samples[..., -q:, :].sum().backward()
+                                test_X2 = test_X.detach().clone().requires_grad_(True)
+                                new_posterior2 = model.posterior(test_X2)
+                                q_samples = sample_cached_cholesky(
+                                    posterior=new_posterior2,
+                                    baseline_L=baseline_L,
+                                    q=q,
+                                    base_samples=sampler.base_samples.detach().clone(),
+                                    sample_shape=sampler.sample_shape,
                                 )
-                            else:
-                                train_X_ex = train_X
-                            if len(test_batch_shape) > 0:
-                                test_X = train_X_ex.unsqueeze(0).expand(
-                                    test_batch_shape + train_X_ex.shape
+                                q_samples.sum().backward()
+                                all_close_kwargs = (
+                                    {
+                                        "atol": 1e-4,
+                                        "rtol": 1e-2,
+                                    }
+                                    if dtype == torch.float
+                                    else {}
                                 )
-                            else:
-                                test_X = train_X_ex
-                            with torch.no_grad():
-                                base_posterior = model.posterior(
-                                    train_X_ex[..., :-q, :]
+                                self.assertTrue(
+                                    torch.allclose(
+                                        q_samples.detach(),
+                                        samples[..., -q:, :].detach(),
+                                        **all_close_kwargs,
+                                    )
                                 )
-                                mvn = base_posterior.mvn
-                                lazy_covar = mvn.lazy_covariance_matrix.base_lazy_tensor
-                                lazy_root = lazy_covar.root_decomposition()
-                                baseline_L = lazy_root.root.evaluate()
-                            test_X = test_X.clone().requires_grad_(True)
-                            new_posterior = model.posterior(test_X)
-                            samples = sampler(new_posterior)
-                            samples[..., -q:, :].sum().backward()
-                            test_X2 = test_X.detach().clone().requires_grad_(True)
-                            new_posterior2 = model.posterior(test_X2)
-                            q_samples = sample_cached_cholesky(
-                                posterior=new_posterior2,
-                                baseline_L=baseline_L,
-                                q=q,
-                                base_samples=sampler.base_samples.detach().clone(),
-                                sample_shape=sampler.sample_shape,
-                            )
-                            q_samples.sum().backward()
-                            all_close_kwargs = (
-                                {
-                                    "atol": 1e-4,
-                                    "rtol": 1e-2,
-                                }
-                                if dtype == torch.float
-                                else {}
-                            )
-                            self.assertTrue(
-                                torch.allclose(
-                                    q_samples.detach(),
-                                    samples[..., -q:, :].detach(),
-                                    **all_close_kwargs,
+                                self.assertTrue(
+                                    torch.allclose(
+                                        test_X2.grad[..., -q:, :],
+                                        test_X.grad[..., -q:, :],
+                                        **all_close_kwargs,
+                                    )
                                 )
-                            )
-                            self.assertTrue(
-                                torch.allclose(
-                                    test_X2.grad[..., -q:, :],
-                                    test_X.grad[..., -q:, :],
-                                    **all_close_kwargs,
-                                )
-                            )
                             # test nans
                             with torch.no_grad():
                                 test_posterior = model.posterior(test_X2)
