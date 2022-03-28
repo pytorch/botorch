73712733aa260dcf72941bbffe591ac1340eb8fb
BoTorch website deployment script <docusaurus-bot@users.noreply.github.com>
2022-03-28 00:55:15 +0000

    Publish version 0.6.3.1 of site

---
+++ v/0.6.3.1/files/bope.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# # Bayesian Optimization with Preference Exploration
+# 
+# In this tutorial, we demonstrate how to implement a closed loop of Bayesian optimization with preference exploration, or BOPE [1].
+# BOPE is designed for Bayesian optimization of expensive-to-evaluate experiments,
+# where the response surface function of the experiment $f_\mathrm{true}$ generates vector-valued outcomes over which a decision-maker (DM) has preferences.
+# These preferences are encoded by a utility function $g_\mathrm{true}$ that is not known in closed form but can be estimated by
+# asking the DM to express preferences over pairs of outcome vectors.
+# 
+# In other words, with BOPE, we wish to solve the following optimization problem:
+# 
+# $$
+#  \max_{x \in \mathcal{X}} g_\mathrm{true}(f_\mathrm{true}(x))
+# $$
+# 
+# Unlike many other Bayesian optimization setups where multiple consecutive batches of experiments are performed,
+# in BOPE, we alternate between two stages: *preference exploration* and *experimentation*.
+# 
+# In the preference exploration stage, we use an acquisition function (i.e., a preference exploratoin strategy, or PE strategy)
+# to adaptively generate pairs of hypothetical outcome and ask the decision-maker’s preference within each pair.
+# In the experimentation stage, we use a batch version of noisy expected improvement that integrates over our uncertainty in the
+# utility function called $\text{qNEIUU}$ to generate experimental candidates for evaluation.
+# 
+# 
+# [1] [Z.J. Lin, R. Astudillo, P.I. Frazier, and E. Bakshy, Preference Exploration for Efficient Bayesian Optimization with Multiple Outcomes. AISTATS, 2022.](https://arxiv.org/abs/2203.11382)
+
+# In[1]:
+
+
+import os
+
+import matplotlib as mpl
+import matplotlib.pylab as plt
+import pandas as pd
+import torch
+from botorch.acquisition import GenericMCObjective, LearnedObjective
+from botorch.acquisition.monte_carlo import qNoisyExpectedImprovement, qSimpleRegret
+from botorch.acquisition.preference import AnalyticExpectedUtilityOfBestOption
+from botorch.fit import fit_gpytorch_model
+from botorch.models.deterministic import FixedSingleSampleModel
+from botorch.models.gp_regression import SingleTaskGP
+from botorch.models.pairwise_gp import PairwiseGP, PairwiseLaplaceMarginalLogLikelihood
+from botorch.optim.optimize import optimize_acqf
+from botorch.sampling.samplers import SobolQMCNormalSampler
+from botorch.test_functions.multi_objective import DTLZ2
+from botorch.utils.sampling import draw_sobol_samples
+from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
+
+
+# Set plotting colors
+colors = ["tab:blue", "tab:orange", "tab:green", "tab:red"]
+mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=colors)
+
+SMOKE_TEST = os.environ.get("SMOKE_TEST")
+
+
+# ## Problem Setup
+# 
+# In this tutorial, we use the DTLZ2 problem with d=6 inputs and k=4 outcomes as our test problem $f_\mathrm{true}$.
+# 
+# For the utility function $g_\mathrm{true}$, we use the negative L1 distance from a Pareto-optimal point the outcome space:
+# $Y^* = f(X^*)$ where $X^* = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]$. 
+
+# In[2]:
+
+
+def neg_l1_dist(Y):
+    """Negative L1 distance from a Pareto optimal points"""
+    if len(Y.shape) == 1:
+        Y = Y.unsqueeze(0)
+    dist = torch.cdist(Y, torch.full(Y.shape[-1:], fill_value=0.5).unsqueeze(0), p=1).squeeze(-1)
+    return -dist
+
+
+if SMOKE_TEST:
+    NUM_RESTARTS = 2
+    NUM_SMAPLES = 8
+    RAW_SAMPLES = 4
+    BATCH_LIMIT = 2
+else:
+    NUM_RESTARTS = 16
+    NUM_SMAPLES = 128
+    RAW_SAMPLES = 128
+    BATCH_LIMIT = 4
+
+X_dim = 6
+Y_dim = 4
+problem = DTLZ2(dim=X_dim, num_objectives=Y_dim)
+util_func = neg_l1_dist
+
+
+# Here we define a collection of helper functions for BOPE:
+
+# In[3]:
+
+
+def fit_outcome_model(X, Y, X_bounds):
+    """Fit the outcome model f"""
+    outcome_model = SingleTaskGP(train_X=X, train_Y=Y)
+    mll = ExactMarginalLogLikelihood(outcome_model.likelihood, outcome_model)
+    fit_gpytorch_model(mll)
+    return outcome_model
+
+
+def fit_pref_model(Y, comps):
+    """Fit the preference model g"""
+    model = PairwiseGP(Y, comps, jitter=1e-4)
+    mll = PairwiseLaplaceMarginalLogLikelihood(model)
+    fit_gpytorch_model(mll)
+    return model
+
+
+def gen_rand_X(problem, n):
+    """Generate n quasi-random Sobol points in the design space"""
+    return draw_sobol_samples(bounds=problem.bounds, n=1, q=n).squeeze(0)
+
+
+def generate_random_exp_data(problem, n):
+    """Generate n observations of (X, Y) Pairs"""
+    X = gen_rand_X(problem, n)
+    Y = problem(X)
+    return X, Y
+
+
+def generate_random_pref_data(outcome_model, n):
+    """Generate n pairwise comparison data between 2n points"""
+    X = gen_rand_X(problem, 2 * n)
+    Y = outcome_model.posterior(X).sample().squeeze(0)
+    util = util_func(Y)
+    comps = gen_comps(util)
+    return Y, comps
+
+
+def gen_comps(util):
+    """Given an 1-d tensor of utility, create pairwise comparisons between adjacent items"""
+    util = util.reshape(-1, 2)
+    comps = torch.arange(util.numel()).reshape(-1, 2)
+    flip = util[:, 0] < util[:, 1]
+    comps[flip, [0]], comps[flip, [1]] = comps[flip, [1]], comps[flip, [0]]
+
+    return comps
+
+
+def gen_exp_cand(outcome_model, objective, q, acqf_name):
+    """Given an outcome model and an objective, generate q experimental candidates
+    using specified acquisition function."""
+    sampler = SobolQMCNormalSampler(num_samples=NUM_SMAPLES)
+    if acqf_name == "qNEI":
+        # generate experimental candidates with qNEI/qNEIUU
+        acq_func = qNoisyExpectedImprovement(
+            model=outcome_model,
+            objective=objective,
+            X_baseline=X,
+            sampler=sampler,
+            prune_baseline=True,
+        )
+    elif acqf_name == "posterior_mean":
+        # generate experimental candidates with maximum posterior mean
+        acq_func = qSimpleRegret(
+            model=outcome_model,
+            sampler=sampler,
+            objective=objective,
+        )
+    else:
+        raise RuntimeError("Unknown acquisition function name!")
+
+    # optimize the acqf
+    candidates, _ = optimize_acqf(
+        acq_function=acq_func,
+        q=q,
+        bounds=problem.bounds,
+        num_restarts=NUM_RESTARTS,
+        raw_samples=RAW_SAMPLES,
+        options={"batch_limit": BATCH_LIMIT},
+        sequential=True,
+    )
+    return candidates
+
+
+def run_pref_learn(outcome_model, train_Y, train_comps, n_comps, pe_strategy):
+    """Perform preference exploration with a given PE strategy for n_comps rounds"""
+    for i in range(n_comps):
+        print(f"Running {i+1}/{n_comps} preference learning using {pe_strategy}")
+        pref_model = fit_pref_model(train_Y, train_comps)
+
+        if pe_strategy == "EUBO-zeta":
+            # EUBO-zeta
+            one_sample_outcome_model = FixedSingleSampleModel(model=outcome_model)
+            acqf = AnalyticExpectedUtilityOfBestOption(pref_model=pref_model, outcome_model=one_sample_outcome_model)
+
+            cand_X, acqf_val = optimize_acqf(
+                acq_function=acqf,
+                q=2,
+                bounds=problem.bounds,
+                num_restarts=NUM_RESTARTS,
+                raw_samples=RAW_SAMPLES,  # used for intialization heuristic
+                options={"batch_limit": BATCH_LIMIT},
+            )
+            cand_Y = one_sample_outcome_model(cand_X)
+        elif pe_strategy == "Random-f":
+            # Random-f
+            cand_X = gen_rand_X(problem, n=2)
+            cand_Y = outcome_model.posterior(cand_X).sample().squeeze(0)
+        else:
+            raise RuntimeError("Unknown preference exploration strategy!")
+
+        cand_Y = cand_Y.detach().clone()
+        cand_comps = gen_comps(util_func(cand_Y))
+
+        train_comps = torch.cat((train_comps, cand_comps + train_Y.shape[0]))
+        train_Y = torch.cat((train_Y, cand_Y))
+
+    return train_Y, train_comps
+
+
+def find_max_posterior_mean(outcome_model, train_Y, train_comps):
+    """Helper function that find the max posterior mean under current outcome and
+    preference model"""
+    pref_model = fit_pref_model(train_Y, train_comps)
+    pref_obj = LearnedObjective(pref_model=pref_model)
+    post_mean_cand_X = gen_exp_cand(outcome_model, pref_obj, q=1, acqf_name="posterior_mean")
+
+    post_mean_util = util_func(problem(post_mean_cand_X)).item()
+    print(f"Max posterior mean utility: {post_mean_util:.3f}")
+    within_result = {
+        "n_comps": train_comps.shape[0],
+        "util": post_mean_util,
+    }
+    return within_result
+
+
+# ## Closed Loop BOPE
+# 
+# ### Setup
+# The following cell shows the core part of this tutorial.
+# In BOPE, we use two probablistic models (in this case, two Gaussian processes) $f$ and $g$
+# to model $f_\mathrm{true}$ and $g_\mathrm{true}$ respectively.
+# 
+# We start by initializing the outcome model $f$ with 16 quasi-random points (the initial experimentation stage).
+# 
+# Next, we enter the preference exploration (PE) stage.
+# A straightforward strategy of performing PE is to present the decision-maker with comparisons using outcomes estimated
+# by sampling from the outcome model using random design points. We refer this method as $\text{Random}\mathrm{-}f$.
+# 
+# Alternatively, we could initialize the preference model with $\text{Random}\mathrm{-}f$,
+# then perform PE using the $\text{EUBO}\mathrm{-}\zeta$ acquisition function as proposed in [1].
+# <!-- \tilde{f} somehow is not rendering --> 
+# 
+# In this tutorial, we examine both strategies by starting with initializating the preference model with 2k=8 comparisons using $\text{Random}\mathrm{-}f$.
+# After that, we perform 20 pairwise comparisons using either $\text{EUBO}\mathrm{-}\zeta$ or $\text{Random}\mathrm{-}f$.
+# Then we move on to the 2nd experimentation stage by generating a candidate using qNEIUU by
+# leveraging both the outcome model and the learned preference model.
+# 
+# We additionally examine two other experimental canadidate generation strategies:
+# In the first one, we assume the true utility function is known and generate experiment candidate(s) using qNEI.
+# This represents the performance upper bound of PE strategies.
+# For the second experiment canadidate generation strategy, we use random design points to generate new candidates.
+
+# In[4]:
+
+
+verbose = False
+every_n_comps = 5
+n_check_post_mean = 4
+n_reps = 2 if SMOKE_TEST else 30
+within_session_results = []
+exp_candidate_results = []
+
+for i in range(n_reps):
+    print(f"Run {i}")
+    # Experimentation stage: initial exploration batch
+    torch.manual_seed(i)
+    X, Y = generate_random_exp_data(problem, 16)
+    outcome_model = fit_outcome_model(X, Y, problem.bounds)
+
+    # Preference exploration stage: initialize the preference model with comparsions
+    # between pairs of outcomes estimated using random design points
+    init_train_Y, init_train_comps = generate_random_pref_data(outcome_model, n=2 * Y_dim)
+
+    # Perform preference exploration using either Random-f or EUBO-zeta
+    for pe_strategy in ["Random-f", "EUBO-zeta"]:
+        train_Y, train_comps = init_train_Y, init_train_comps
+        within_result = find_max_posterior_mean(outcome_model, train_Y, train_comps)
+        within_result.update({"run_id": i, "pe_strategy": pe_strategy})
+        within_session_results.append(within_result)
+
+        for j in range(n_check_post_mean):
+            train_Y, train_comps = run_pref_learn(
+                outcome_model, train_Y, train_comps, n_comps=every_n_comps, pe_strategy=pe_strategy
+            )
+            if verbose:
+                print(f"Checking posterior mean after {(j+1) * every_n_comps} comps using PE strategy {pe_strategy}")
+            within_result = find_max_posterior_mean(outcome_model, train_Y, train_comps)
+            within_result.update({"run_id": i, "pe_strategy": pe_strategy})
+            within_session_results.append(within_result)
+
+        # Going back to the experimentation stage: generate an additional batch of experimental evaluations
+        # with the learned preference model and qNEIUU
+        pref_model = fit_pref_model(train_Y, train_comps)
+        pref_obj = LearnedObjective(pref_model=pref_model)
+        exp_cand_X = gen_exp_cand(outcome_model, pref_obj, q=1, acqf_name="qNEI")
+        qneiuu_util = util_func(problem(exp_cand_X)).item()
+        if verbose:
+            print(f"qNEIUU candidate utility: {qneiuu_util:.3f}")
+        exp_result = {
+            "util": qneiuu_util,
+            "strategy": pe_strategy,
+            "run_id": i,
+        }
+        exp_candidate_results.append(exp_result)
+
+    # Generate a batch of experimental evaluations using oracle and random baselines
+    # True utility
+    true_obj = GenericMCObjective(util_func)
+    true_obj_cand_X = gen_exp_cand(outcome_model, true_obj, q=1, acqf_name="qNEI")
+    true_obj_util = util_func(problem(true_obj_cand_X)).item()
+    if verbose:
+        print(f"True objective utility: {true_obj_util:.3f}")
+    exp_result = {
+        "util": true_obj_util,
+        "strategy": "True Utility",
+        "run_id": i,
+    }
+    exp_candidate_results.append(exp_result)
+
+    # Random experiment
+    _, random_Y = generate_random_exp_data(problem, 1)
+    random_util = util_func(random_Y).item()
+    if verbose:
+        print(f"Random experiment utility: {random_util:.3f}")
+    exp_result = {
+        "util": random_util,
+        "strategy": "Random Experiment",
+        "run_id": i,
+    }
+    exp_candidate_results.append(exp_result)
+
+
+# ## Plot the Results
+# 
+# We evaluate our results by creating two plots.
+# 
+# In the first plot, we focus on comparing how $\text{EUBO}\mathrm{-}\zeta$ can efficiently identify the maximizer 
+# of $g_\mathrm{true}(f_\mathrm{true}(x))$ within a preference exploration stage.
+# We examine this by estimatig the maximum posterior mean after every 5 pairwise comparisons.
+# 
+# Here, we plot the the max utility value identified using $\text{EUBO}\mathrm{-}\zeta$ and $\text{Random}\mathrm{-}f$
+# with increasing number of pairwise comparisons.
+# As we can see in this plot, the preference model learned using $\text{EUBO}\mathrm{-}\zeta$ is able to identify the maximum utility more efficiently.
+
+# In[12]:
+
+
+# Prepare PE data for plots
+within_df = pd.DataFrame(within_session_results)
+within_df["pe_strategy"] = within_df["pe_strategy"].str.replace("EUBO-zeta", r"$EUBO-\\zeta$")
+within_df = (
+    within_df.groupby(["n_comps", "pe_strategy"])
+    .agg({"util": ["mean", "sem"]})
+    .droplevel(level=0, axis=1)
+    .reset_index()
+)
+within_df
+
+# Plotting
+plt.figure(figsize=(8, 6))
+for name, group in within_df.groupby("pe_strategy", sort=True):
+    plt.errorbar(
+        x=group["n_comps"], y=group["mean"], yerr=1.96 * group["sem"], label=name, linewidth=1.5, capsize=3, alpha=0.6
+    )
+plt.xlabel("Number of comparisons")
+plt.ylabel("Max value identified")
+plt.legend(title="PE Strategy")
+
+
+# In the following cell, we show the box plot of utility values achieved using different methods in the 2nd experimentation stage.
+# 
+# We see that $\text{EUBO}\mathrm{-}\zeta$, as a one-step Bayesian optimal PE strategy, performs very similarly to the true utility strategy.
+# 
+# On the other hand, despite that $\text{Random}\mathrm{-}f$ is a relatively straightforward PE strategy,
+# it is still able to suggest experimental candidates with generally higher utility values than the random experiment baseline,
+# demonstrating the efficacy of the BOPE framework.
+
+# In[16]:
+
+
+# Prepare the 2nd experimentation batch data for plot
+exp_df = pd.DataFrame(exp_candidate_results)
+exp_df["strategy"] = exp_df["strategy"].str.replace("EUBO-zeta", r"$EUBO-\\zeta$")
+exp_df["strategy"] = pd.Categorical(
+    exp_df["strategy"], ["True Utility", "$EUBO-\zeta$", "Random-f", "Random Experiment"]
+)
+bp_data = []
+labels = []
+for name, group in exp_df.groupby("strategy", sort=True):
+    bp_data.append(group["util"])
+    labels.append(name)
+
+# Plotting
+plt.figure(figsize=(9, 6))
+bp = plt.boxplot(bp_data, labels=labels, patch_artist=True)
+for median in bp["medians"]:
+    median.set_color("black")
+for patch, color in zip(bp["boxes"], colors):
+    patch.set_facecolor(color)
+
+plt.xlabel("Strategy")
+plt.ylabel("Utility achieved in the 2nd experiment stage")
+
---
+++ v/0.6.3.1/files/saasbo.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# ## High-Dimensional sample-efficient Bayesian Optimization with SAASBO
+# 
+# This tutorial shows how to use the Sparse Axis-Aligned Subspace Bayesian Optimization (SAASBO) 
+# method for high-dimensional Bayesian optimization [1]. SAASBO places strong priors on the 
+# inverse lengthscales to avoid overfitting in high-dimensional spaces. Specifically, SAASBO 
+# uses a hierarchical sparsity prior consisting of a global shrinkage parameter 
+# $\tau \sim \mathcal{HC}(\beta)$ and inverse lengthscales $\rho_d \sim \mathcal{HC}(\tau)$ 
+# for $d=1, \ldots, D$, where $\mathcal{HC}$ is the half-Cauchy distribution. 
+# While half-Cauchy priors favor values near zero they also have heavy tails, which allows the 
+# inverse lengthscales of the most important parameters to escape zero. To perform inference in the 
+# SAAS model we use Hamiltonian Monte Carlo (HMC) as we found that to outperform MAP inference.
+# 
+# We find that SAASBO performs well on problems with hundreds of dimensions. As we rely on HMC 
+# and in particular the No-U-Turn-Sampler (NUTS) for inference, the overhead of SAASBO scales 
+# cubically with the number of datapoints. Depending on the problem, using more than a few hundred
+# evaluations may not be feasible as SAASBO is designed for problems with a limited evaluation budget.
+# 
+# [1]: [D. Eriksson, M. Jankowiak. High-Dimensional Bayesian Optimization with Sparse Axis-Aligned Subspaces. Proceedings of the Thirty-Seventh Conference on Uncertainty in Artificial Intelligence, 2021.](https://proceedings.mlr.press/v161/eriksson21a.html)
+
+# In[1]:
+
+
+import os
+
+import torch
+from botorch import fit_fully_bayesian_model_nuts
+from botorch.acquisition import qExpectedImprovement
+from botorch.models.fully_bayesian import SaasFullyBayesianSingleTaskGP
+from botorch.models.transforms import Standardize
+from botorch.optim import optimize_acqf
+from botorch.test_functions import Branin
+from torch.quasirandom import SobolEngine
+
+
+SMOKE_TEST = os.environ.get("SMOKE_TEST")
+
+
+# In[2]:
+
+
+tkwargs = {"device": torch.device("cuda" if torch.cuda.is_available() else "cpu"), "dtype": torch.double}
+
+
+# ## Simple model fitting
+# We generate a simple function that only depends no the first parameter and show that the SAAS
+# model sets all other lengthscale to large values.
+
+# In[3]:
+
+
+train_X = torch.rand(10, 4, **tkwargs)
+test_X = torch.rand(5, 4, **tkwargs)
+train_Y = torch.sin(train_X[:, :1])
+test_Y = torch.sin(test_X[:, :1])
+
+
+# In[4]:
+
+
+saas_gp = SaasFullyBayesianSingleTaskGP(train_X=train_X, train_Y=train_Y)
+fit_fully_bayesian_model_nuts(saas_gp)
+with torch.no_grad():
+    posterior = saas_gp.posterior(test_X, marginalize_over_mcmc_samples=True)
+
+
+# Test predictions are very close to the ground-truth
+
+# In[5]:
+
+
+print(posterior.mean.squeeze(-1))
+print(test_Y.squeeze(-1))
+
+
+# The first feature has the smallest lengthscale
+
+# In[6]:
+
+
+print(saas_gp.median_lengthscale)
+
+
+# ## Model fitting with fixed noise
+# By default, we infer the unknown noise variance in the data. You can also pass in a known 
+# noise variance for each observation, which may be useful in cases where you for example
+# know that the problem is noise-free and can then set the noise variance to a small value such as `1e-6`.
+
+# In[7]:
+
+
+saas_gp = SaasFullyBayesianSingleTaskGP(train_X=train_X, train_Y=train_Y, train_Yvar=torch.full_like(train_Y, 1e-6))
+fit_fully_bayesian_model_nuts(saas_gp)
+with torch.no_grad():
+    posterior = saas_gp.posterior(test_X, marginalize_over_mcmc_samples=True)
+
+
+# In[8]:
+
+
+print(posterior.mean.squeeze(-1))
+print(test_Y.squeeze(-1))
+
+
+# In[9]:
+
+
+print(saas_gp.median_lengthscale)
+
+
+# ## Optimize Branin embedded in a 50D space
+# We take the standard 2D Branin problem and embed it in a 50D space. In particular,
+# we let dimensions 0 and 1 correspond to the true dimensions. We will show that
+# SAASBO is able to identify the important dimensions and efficiently optimize this function.
+# We work with the domain $[0, 1]^d$ and unnormalize the inputs to the true domain of Branin 
+# before evaluating the function.
+# 
+# The time to fit the SAAS model can be decreased by lowering
+# `WARMUP_STEPS` and `NUM_SAMPLES`. We recommend using 512 warmup steps and 256 samples when
+# possible and to not use fewer than 256 warmup steps and 128 samples. By default, we only
+# keep each 16th sample which with 256 samples results in 32 hyperparameter samples.
+# We average over these samples when computing the acquisition function.
+
+# In[10]:
+
+
+branin = Branin().to(**tkwargs)
+
+
+def branin_emb(x):
+    """x is assumed to be in [0, 1]^d"""
+    lb, ub = branin.bounds
+    return branin(lb + (ub - lb) * x[..., :2])
+
+
+# In[11]:
+
+
+DIM = 50 if not SMOKE_TEST else 10
+
+# Evaluation budget
+N_INIT = 10
+N_ITERATIONS = 10 if not SMOKE_TEST else 1
+BATCH_SIZE = 4 if not SMOKE_TEST else 1
+print(f"Using a total of {N_INIT + BATCH_SIZE * N_ITERATIONS} function evaluations")
+
+# Settings for NUTS
+WARMUP_STEPS = 512 if not SMOKE_TEST else 32
+NUM_SAMPLES = 256 if not SMOKE_TEST else 16
+THINNING = 16
+
+
+# ### Run the optimization
+# We use 10 initial Sobol points followed by 10 iterations of BO using a batch size of 4, 
+# which results in a total of 50 function evaluations. As our goal is to minimize Branin, we flip
+# the sign of the function values before fitting the SAAS model as the BoTorch acquisition
+# functions assume maximization.
+
+# In[12]:
+
+
+X = SobolEngine(dimension=DIM, scramble=True, seed=0).draw(N_INIT).to(**tkwargs)
+Y = branin_emb(X).unsqueeze(-1)
+print(f"Best initial point: {Y.min().item():.3f}")
+
+for i in range(N_ITERATIONS):
+    train_Y = -1 * Y  # Flip the sign since we want to minimize f(x)
+    gp = SaasFullyBayesianSingleTaskGP(
+        train_X=X, train_Y=train_Y, train_Yvar=torch.full_like(train_Y, 1e-6), outcome_transform=Standardize(m=1)
+    )
+    fit_fully_bayesian_model_nuts(gp, warmup_steps=WARMUP_STEPS, num_samples=NUM_SAMPLES, disable_progbar=True)
+
+    EI = qExpectedImprovement(model=gp, best_f=train_Y.max())
+    candidates, acq_values = optimize_acqf(
+        EI,
+        bounds=torch.cat((torch.zeros(1, DIM), torch.ones(1, DIM))).to(**tkwargs),
+        q=BATCH_SIZE,
+        num_restarts=10,
+        raw_samples=1024,
+    )
+
+    Y_next = torch.cat([branin_emb(x).unsqueeze(-1) for x in candidates]).unsqueeze(-1)
+    if Y_next.min() < Y.min():
+        ind_best = Y_next.argmin()
+        x0, x1 = candidates[ind_best, :2].tolist()
+        print(f"{i + 1}) New best: {Y_next[ind_best].item():.3f} @ " f"[{x0:.3f}, {x1:.3f}]")
+    X = torch.cat((X, candidates))
+    Y = torch.cat((Y, Y_next))
+
+
+# ## Plot the results
+# 
+# We can see that we were able to get close to the global optimium of $\approx 0.398$ after 50 function evaluations.
+# 
+
+# In[13]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+Y_np = Y.cpu().numpy()
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(np.minimum.accumulate(Y_np), color="b", label="SAASBO")
+ax.plot([0, len(Y_np)], [0.398, 0.398], "--", c="g", lw=3, label="Optimal value")
+ax.grid(True)
+ax.set_title(f"Branin, D = {DIM}", fontsize=20)
+ax.set_xlabel("Number of evaluations", fontsize=20)
+ax.set_xlim([0, len(Y_np)])
+ax.set_ylabel("Best value found", fontsize=20)
+ax.set_ylim([0, 8])
+ax.legend(fontsize=18)
+plt.show()
+
+
+# ## Look a the lengthscales from the final model
+# 
+# As SAASBO places strong priors on the inverse lengthscales, we only expect parameters 
+# 0 and 1 to be identified as important by the model since the other parameters have no effect.
+# We can confirm that this is the case below as the lengthscales of parameters 0 and 1 are 
+# small with all other lengthscales being large.
+
+# In[14]:
+
+
+median_lengthscales = gp.median_lengthscale
+for i in median_lengthscales.argsort()[:10]:
+    print(f"Parameter {i:2}) Median lengthscale = {median_lengthscales[i].item():.2e}")
+
+
+# In[14]:
+
+
+
+
