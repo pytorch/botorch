f4989093fffb32ed67ed10c44004ba4876ebe1cb
wjmaddox <wjm363@nyu.edu>
2022-04-12 10:22:32 -0400

    in docstring; not grad

--- botorch/acquisition/acquisition.py
+++ botorch/acquisition/acquisition.py
@@ -22,14 +22,15 @@ from torch.nn import Module
 
 
 class AcquisitionFunction(Module, ABC):
-    r"""Abstract base class for acquisition functions."""
-
-    def __init__(self, model: Model) -> None:
-        r"""Constructor for the AcquisitionFunction base class.
+    r"""Abstract base class for acquisition functions. 
 
         Please note that if your acquisition requires a backwards call,
         you will need to wrap the backwards call inside of an enable_grad
         context to be able to optimize the acquisition. See #1164.
+    """
+
+    def __init__(self, model: Model) -> None:
+        r"""Constructor for the AcquisitionFunction base class.
 
         Args:
             model: A fitted model.
