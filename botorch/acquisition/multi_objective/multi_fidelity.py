1b92ee84ae3c66935aba85f52758f2c1ba4c5c33
faranirshad <51855572+faranirshad@users.noreply.github.com>
2022-05-16 16:53:23 -0700

    Adding MOMF acquisition function (#1153)
    Summary:
    <!--
    Thank you for sending the PR! We appreciate you spending the time to make BoTorch better.
    
    Help us understand your motivation by explaining why you decided to make this change.
    
    You can learn more about contributing to BoTorch here: https://github.com/pytorch/botorch/blob/main/CONTRIBUTING.md
    -->
    
    ## Motivation
    
    The MOMF acquisition function is targeted at simultaneously optimizing a Multi-objective Multi-Fidelity problem. It does this by making use of a 'Trust Objective' which is optimized along with the other multi-objectives. This allows the MOMF acquisition function to balance the trade-off of going for the cheaper objective evaluations vs yielding higher values for Trust objective by going to higher fidelity evaluations. This PR includes an implementation of the MOMF acquisition function, new Multi-objective Multi-Fidelity test functions and a tutorial jupyter notebook that walks through the procedure of using MOMF to optimize a Modified Branin-Currin problem. For more details on the working of the acquisition function see
    [Irshad, F., Karsch, S. and Döpp, A., 2021. Expected hypervolume improvement for simultaneous multi-objective and multi-fidelity optimization. arXiv preprint arXiv:2112.13901].
    
    ### Have you read the [Contributing Guidelines on pull requests](https://github.com/pytorch/botorch/blob/main/CONTRIBUTING.md#pull-requests)?
    
    Yes I have and tried to adhere to all the guidelines mentioned.
    
    Pull Request resolved: https://github.com/pytorch/botorch/pull/1153
    
    Test Plan:
    For the MOMF acquisition function based on qEHVI, I made use of some of the test cases of the qEHVI and rewrote some parts of it. The changes include new values for acquisition function values since the MOMF internally divides by the cost of evaluating points and so the expected values from the test cases were changed to account for this. I wrote a new test case as well where I control the fidelity cost and check if the MOMF returns the true accurate value within a certain tolerance. The MOMF is also checked for returning the right tensor shapes in case of q=1,2,3.
    The test functions also undergo a similar check where they are checked for correct initialization and returning the optimized values when evaluated at optimizers.
    
    ## Related PRs
    
    This does adds functionality and so I have created a jupyter tutorial notebook to outline this functionality. It is a part of this PR.
    (If this PR adds or changes functionality, please take some time to update the docs at https://github.com/pytorch/botorch, and link to your PR here.)
    
    Reviewed By: sdaulton
    
    Differential Revision: D36398219
    
    Pulled By: Balandat
    
    fbshipit-source-id: 02b9711f650b017c46a4108451e4e4786b537d87

---
+++ botorch/acquisition/multi_objective/multi_fidelity.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+r"""
+Multi-Fidelity Acquisition Functions for Multi-objective Bayesian optimization.
+
+References
+
+.. [Irshad2021MOMF]
+    F. Irshad, S. Karsch, and A. Döpp. Expected hypervolume improvement for
+    simultaneous multi-objective and multi-fidelity optimization.
+    arXiv preprint arXiv:2112.13901, 2021.
+
+"""
+
+from __future__ import annotations
+
+from typing import Any, Callable, List, Optional, Union
+
+import torch
+from botorch.acquisition.cost_aware import InverseCostWeightedUtility
+from botorch.acquisition.multi_objective.monte_carlo import (
+    qExpectedHypervolumeImprovement,
+)
+from botorch.acquisition.multi_objective.objective import MCMultiOutputObjective
+from botorch.models.cost import AffineFidelityCostModel
+from botorch.models.deterministic import GenericDeterministicModel
+from botorch.models.model import Model
+from botorch.sampling.samplers import MCSampler
+from botorch.utils.multi_objective.box_decompositions.non_dominated import (
+    NondominatedPartitioning,
+)
+from botorch.utils.transforms import concatenate_pending_points, t_batch_mode_transform
+from torch import Tensor
+
+
+class MOMF(qExpectedHypervolumeImprovement):
+    def __init__(
+        self,
+        model: Model,
+        ref_point: Union[List[float], Tensor],
+        partitioning: NondominatedPartitioning,
+        sampler: Optional[MCSampler] = None,
+        objective: Optional[MCMultiOutputObjective] = None,
+        constraints: Optional[List[Callable[[Tensor], Tensor]]] = None,
+        X_pending: Optional[Tensor] = None,
+        cost_call: Callable[Tensor, Tensor] = None,
+        eta: float = 1e-3,
+        **kwargs: Any,
+    ) -> None:
+        r"""MOMF acquisition function supporting m>=2 outcomes.
+        The model needs to have train_obj that has a fidelity
+        objective appended to its end.
+        In the following example we consider a 2-D output space
+        but the ref_point is 3D because of fidelity objective.
+
+        See [Irshad2021MOMF]_ for details.
+
+        Example:
+            >>> model = SingleTaskGP(train_X, train_Y)
+            >>> ref_point = [0.0, 0.0, 0.0]
+            >>> cost_func = lambda X: 5 + X[..., -1]
+            >>> momf = MOMF(model, ref_point, partitioning, cost_func)
+            >>> momf_val = momf(test_X)
+
+        Args:
+            model: A fitted model. There are two default assumptions in the training
+                data. `train_X` should have fidelity parameter `s` as the last dimension
+                of the input and `train_Y` contains a trust objective as its last
+                dimension.
+            ref_point: A list or tensor with `m+1` elements representing the reference
+                point (in the outcome space) w.r.t. to which compute the hypervolume.
+                The '+1' takes care of the trust objective appended to `train_Y`.
+                This is a reference point for the objective values (i.e. after
+                applying`objective` to the samples).
+            partitioning: A `NondominatedPartitioning` module that provides the non-
+                dominated front and a partitioning of the non-dominated space in hyper-
+                rectangles. If constraints are present, this partitioning must only
+                include feasible points.
+            sampler: The sampler used to draw base samples. Defaults to
+                `SobolQMCNormalSampler(num_samples=128, collapse_batch_dims=True)`.
+            objective: The MCMultiOutputObjective under which the samples are evaluated.
+                Defaults to `IdentityMultiOutputObjective()`.
+            constraints: A list of callables, each mapping a Tensor of dimension
+                `sample_shape x batch-shape x q x m` to a Tensor of dimension
+                `sample_shape x batch-shape x q`, where negative values imply
+                feasibility. The acquisition function will compute expected feasible
+                hypervolume.
+            X_pending: A `batch_shape x m x d`-dim Tensor of `m` design points that have
+                points that have been submitted for function evaluation but have not yet
+                been evaluated. Concatenated into `X` upon forward call. Copied and set
+                to have no gradient.
+            cost_call: A callable cost function mapping a Tensor of dimension
+                `batch_shape x q x d` to a cost Tensor of dimension
+                `batch_shape x q x m`. Defaults to an AffineCostModel with
+                `C(s) = 1 + s`.
+            eta: The temperature parameter for the sigmoid function used for the
+                differentiable approximation of the constraints.
+        """
+
+        if len(ref_point) != partitioning.num_outcomes:
+            raise ValueError(
+                "The length of the reference point must match the number of outcomes. "
+                f"Got ref_point with {len(ref_point)} elements, but expected "
+                f"{partitioning.num_outcomes}."
+            )
+        ref_point = torch.as_tensor(
+            ref_point,
+            dtype=partitioning.pareto_Y.dtype,
+            device=partitioning.pareto_Y.device,
+        )
+        super().__init__(
+            model=model,
+            ref_point=ref_point,
+            partitioning=partitioning,
+            sampler=sampler,
+            objective=objective,
+            constraints=constraints,
+            X_pending=X_pending,
+        )
+
+        if cost_call is None:
+            cost_model = AffineFidelityCostModel(
+                fidelity_weights={-1: 1.0}, fixed_cost=1.0
+            )
+        else:
+            cost_model = GenericDeterministicModel(cost_call)
+        cost_aware_utility = InverseCostWeightedUtility(cost_model=cost_model)
+        self.cost_aware_utility = cost_aware_utility
+
+    @concatenate_pending_points
+    @t_batch_mode_transform()
+    def forward(self, X: Tensor) -> Tensor:
+        posterior = self.model.posterior(X)
+        samples = self.sampler(posterior)
+        hv_gain = self._compute_qehvi(samples=samples, X=X)
+        cost_weighted_qehvi = self.cost_aware_utility(X=X, deltas=hv_gain)
+        return cost_weighted_qehvi
