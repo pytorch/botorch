818a363ce45960d3f8a6da4822a51e2961109e30
Sait Cakmak <saitcakmak@fb.com>
2022-06-06 19:55:30 -0700

    Fix `ModelListGP.condition_on_observations/fantasize` bug (#1250)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/1250
    
    `ModelListGP.fantasize` would fail due to `X` being transformed into a list of `X` in `fantasize`, then again being made into a list of `X`s in `condition_on_observations`. This removes the duplication of `X` in condition on observations, using the correctly transformed `X`s for fantasizing.
    
    Fixes #1247
    
    Differential Revision: D36949105
    
    fbshipit-source-id: 59e9c11f3ba9e5bf32c1e8e5b9cb8e8ca76cba07

--- test/models/test_model_list_gp_regression.py
+++ test/models/test_model_list_gp_regression.py
@@ -17,6 +17,7 @@ from botorch.models.gp_regression import FixedNoiseGP, SingleTaskGP
 from botorch.models.transforms import Standardize
 from botorch.models.transforms.input import Normalize
 from botorch.posteriors import GPyTorchPosterior
+from botorch.sampling.samplers import IIDNormalSampler
 from botorch.utils.testing import _get_random_data, BotorchTestCase
 from gpytorch.distributions import MultitaskMultivariateNormal, MultivariateNormal
 from gpytorch.kernels import MaternKernel, ScaleKernel
@@ -142,19 +143,19 @@ class TestModelListGP(BotorchTestCase):
             self.assertIsInstance(posterior.mvn, MultivariateNormal)
 
             # test condition_on_observations
-            f_x = torch.rand(2, 1, **tkwargs)
+            f_x = [torch.rand(2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(2, 2, **tkwargs)
             cm = model.condition_on_observations(f_x, f_y)
             self.assertIsInstance(cm, ModelListGP)
 
             # test condition_on_observations batched
-            f_x = torch.rand(3, 2, 1, **tkwargs)
+            f_x = [torch.rand(3, 2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(3, 2, 2, **tkwargs)
             cm = model.condition_on_observations(f_x, f_y)
             self.assertIsInstance(cm, ModelListGP)
 
             # test condition_on_observations batched (fast fantasies)
-            f_x = torch.rand(2, 1, **tkwargs)
+            f_x = [torch.rand(2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(3, 2, 2, **tkwargs)
             cm = model.condition_on_observations(f_x, f_y)
             self.assertIsInstance(cm, ModelListGP)
@@ -162,6 +163,10 @@ class TestModelListGP(BotorchTestCase):
             # test condition_on_observations (incorrect input shape error)
             with self.assertRaises(BotorchTensorDimensionError):
                 model.condition_on_observations(f_x, torch.rand(3, 2, 3, **tkwargs))
+
+            # test X having wrong size
+            with self.assertRaises(AssertionError):
+                cm = model.condition_on_observations(f_x[:1], f_y)
 
             # test posterior transform
             X = torch.rand(3, 1, **tkwargs)
@@ -222,21 +227,21 @@ class TestModelListGP(BotorchTestCase):
             self.assertIsInstance(posterior.mvn, MultivariateNormal)
 
             # test condition_on_observations
-            f_x = torch.rand(2, 1, **tkwargs)
+            f_x = [torch.rand(2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(2, 2, **tkwargs)
             noise = 0.1 + 0.1 * torch.rand_like(f_y)
             cm = model.condition_on_observations(f_x, f_y, noise=noise)
             self.assertIsInstance(cm, ModelListGP)
 
             # test condition_on_observations batched
-            f_x = torch.rand(3, 2, 1, **tkwargs)
+            f_x = [torch.rand(3, 2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(3, 2, 2, **tkwargs)
             noise = 0.1 + 0.1 * torch.rand_like(f_y)
             cm = model.condition_on_observations(f_x, f_y, noise=noise)
             self.assertIsInstance(cm, ModelListGP)
 
             # test condition_on_observations batched (fast fantasies)
-            f_x = torch.rand(2, 1, **tkwargs)
+            f_x = [torch.rand(2, 1, **tkwargs) for _ in range(2)]
             f_y = torch.rand(3, 2, 2, **tkwargs)
             noise = 0.1 + 0.1 * torch.rand(2, 2, **tkwargs)
             cm = model.condition_on_observations(f_x, f_y, noise=noise)
@@ -295,3 +300,15 @@ class TestModelListGP(BotorchTestCase):
             )
             self.assertTrue(m._has_transformed_inputs)
             self.assertTrue(torch.equal(m._original_train_inputs, org_inputs[i]))
+
+    def test_fantasize(self):
+        m1 = SingleTaskGP(torch.rand(5, 2), torch.rand(5, 1)).eval()
+        m2 = SingleTaskGP(torch.rand(5, 2), torch.rand(5, 1)).eval()
+        modellist = ModelListGP(m1, m2)
+        fm = modellist.fantasize(torch.rand(3, 2), sampler=IIDNormalSampler(2))
+        self.assertIsInstance(fm, ModelListGP)
+        for i in range(2):
+            fm_i = fm.models[i]
+            self.assertIsInstance(fm_i, SingleTaskGP)
+            self.assertEqual(fm_i.train_inputs[0].shape, torch.Size([2, 8, 2]))
+            self.assertEqual(fm_i.train_targets.shape, torch.Size([2, 8]))
