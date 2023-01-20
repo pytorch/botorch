236a7dd6c1b1e203b3c471a439d35bb4e38cad38
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-01-20 19:48:02 +0100

    run ufmt

--- test/posteriors/test_ensemble.py
+++ test/posteriors/test_ensemble.py
@@ -4,10 +4,11 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import itertools
+
 import torch
 from botorch.posteriors.ensemble import EnsemblePosterior
 from botorch.utils.testing import BotorchTestCase
-import itertools
 
 
 class TestEnsemblePosterior(BotorchTestCase):
