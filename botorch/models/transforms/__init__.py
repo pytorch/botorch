fc1770d1b08254ad42577cb6994f3f58269db856
Sam Daulton <sdaulton@meta.com>
2023-02-06 11:41:48 -0800

    add utility for constructing rounding input transforms (#1531)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/1531
    
    see title
    
    Reviewed By: Balandat
    
    Differential Revision: D41497584
    
    fbshipit-source-id: a3294a28e1ccec48e1e19a0d0685403a3aa4b7e6

--- botorch/models/transforms/__init__.py
+++ botorch/models/transforms/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from botorch.models.transforms.factory import get_rounding_input_transform
 from botorch.models.transforms.input import (
     ChainedInputTransform,
     Normalize,
@@ -20,6 +21,7 @@ from botorch.models.transforms.outcome import (
 
 
 __all__ = [
+    "get_rounding_input_transform",
     "Bilog",
     "ChainedInputTransform",
     "ChainedOutcomeTransform",
