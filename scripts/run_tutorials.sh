ef86546f2e297e813ce99116ee925380b1e0a55f
Elizabeth Santorella <santorella@meta.com>
2023-02-17 16:30:35 -0500

    test script to write tutorials output to an artifacts branch

---
+++ scripts/run_tutorials.sh
@@ -0,0 +1,16 @@
+echo "cloning"
+git clone https://docusaurus-bot@github.com/pytorch/botorch.git botorch-main
+cd botorch-main
+echo "creating file"
+touch test_file.csv
+echo "Checking out branch artifacts"
+git fetch origin artifacts
+git checkout artifacts
+git add test_file.csv
+echo "Committing"
+git commit test_file.csv -m "Adding most recent tutorials output"
+echo "Pushing"
+git push origin artifacts
+# clean up
+cd ..
+rm -rf botorch-main 
