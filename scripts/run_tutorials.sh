8e5eed6f94020ebd3680a6b6b0aff36ecb99ac76
Elizabeth Santorella <santorella@meta.com>
2023-02-20 12:27:45 -0500

    License; use secrets.GITHUB_TOKEN

--- scripts/run_tutorials.sh
+++ scripts/run_tutorials.sh
@@ -1,18 +1,24 @@
-git config --global user.email "santorella@meta.com"
-git config --global user.name "Elizabeth Santorella"
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 echo "cloning"
 git clone https://github.com/pytorch/botorch.git botorch-main
 cd botorch-main
 echo "creating file"
-touch test_file.csv
+touch test_file0.csv
+
 echo "Checking out branch artifacts"
-git fetch origin artifacts
-git checkout artifacts
-git add test_file.csv
-echo "Committing"
-git commit test_file.csv -m "Adding most recent tutorials output"
-echo "Pushing"
-git push origin artifacts
-# clean up
+git fetch origin artifacts &&
+    git checkout artifacts &&
+    git add test_file0.csv &&
+    echo "Committing and pushing" && 
+    git commit test_file0.csv -m "Adding most recent tutorials output" &&
+    echo "Pushing" &&
+    git push origin artifacts
+
+echo "Cleaning up"
 cd ..
 rm -rf botorch-main 
