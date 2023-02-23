15f00b3e544a2e5a1e7e7d49d13304a215cc6af5
Elizabeth Santorella <santorella@meta.com>
2023-02-23 15:12:26 -0800

    Minor type fixes (#1693)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/1693
    
    Fixing some minor issues Pyre pointed out that I spotted while looking at D43001106
    
    Reviewed By: saitcakmak
    
    Differential Revision: D43440352
    
    fbshipit-source-id: b1a2729ee5db8e48dca66f70cf0211f5bf06e9de

--- botorch/models/utils/inducing_point_allocators.py
+++ botorch/models/utils/inducing_point_allocators.py
@@ -51,8 +51,6 @@ class InducingPointAllocator(ABC):
             A quality function.
         """
 
-        pass  # pragma: no cover
-
     def allocate_inducing_points(
         self,
         inputs: Tensor,
@@ -88,7 +86,6 @@ class InducingPointAllocator(ABC):
             )
         # multi-task case
         elif train_train_kernel.ndimension() == 3 and len(input_batch_shape) == 0:
-            quality_scores = quality_function(inputs)
             input_element = inputs[0] if inputs.ndimension() == 3 else inputs
             kernel_element = train_train_kernel[0]
             quality_scores = quality_function(input_element)
@@ -146,8 +143,6 @@ class QualityFunction(ABC):
         Returns:
             A tensor of quality scores for each input, of shape [n]
         """
-
-        pass  # pragma: no cover
 
 
 class UnitQualityFunction(QualityFunction):
@@ -313,7 +308,7 @@ def _pivoted_cholesky_init(
         (max_length, item_size), device=kernel_matrix.device, dtype=kernel_matrix.dtype
     )
     di2s = kernel_matrix.diag()
-    scores = di2s * (quality_scores**2)
+    scores = di2s * torch.square(quality_scores)
     selected_items = []
     selected_item = torch.argmax(scores)
     selected_items.append(selected_item)
@@ -327,7 +322,7 @@ def _pivoted_cholesky_init(
         cis[k, :] = eis
         di2s = di2s - eis.pow(2.0)
         di2s[selected_item] = NEG_INF
-        scores = di2s * (quality_scores**2)
+        scores = di2s * torch.square(quality_scores)
         selected_item = torch.argmax(scores)
         if di2s[selected_item] < epsilon:
             break
