23ef9eb8810770f661ed498bd629eea5f90fa497
Elizabeth Santorella <santorella@meta.com>
2023-02-27 16:07:20 -0500

    lint

--- scripts/analyze_tutorials_performance.py
+++ scripts/analyze_tutorials_performance.py
@@ -3,6 +3,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 """
 The "artifacts" branch stores CSVs of tutorial runtime and memory:
 https://github.com/pytorch/botorch/tree/artifacts/tutorial_performance_data
