a2cf48109afb37d26ba887a48a8d747706245eb6
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-03-09 07:53:27 +0100

    fix bug

--- botorch/acquisition/multi_objective/objective.py
+++ botorch/acquisition/multi_objective/objective.py
@@ -188,7 +188,7 @@ class FeasibilityWeightedMCMultiOutputObjective(MCMultiOutputObjective):
             from botorch.acquisition.utils import get_infeasible_cost
 
             inf_cost = get_infeasible_cost(
-                X=X_baseline, model=model, objective=lambda y, X_baseline: y
+                X=X_baseline, model=model, objective=lambda y, X: y
             )[objective_idcs]
 
             def apply_feasibility_weights(
