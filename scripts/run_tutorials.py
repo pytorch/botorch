feb0f0418d1ad0125d1252df4d40697862715132
David Eriksson <deriksson@fb.com>
2023-03-15 11:13:23 -0600

    Drop the number of candidates

--- scripts/run_tutorials.py
+++ scripts/run_tutorials.py
@@ -27,8 +27,6 @@ IGNORE_ALWAYS = {  # ignored in smoke tests and full runs
 RUN_IF_SMOKE_TEST_IGNORE_IF_STANDARD = {  # only used in smoke tests
     "thompson_sampling.ipynb",  # very slow without KeOps + GPU
     "composite_mtbo.ipynb",  # TODO: very slow, figure out if we can make it faster
-    # Timing out in standard mode
-    "scalable_constrained_bo.ipynb",
 }
 
 
