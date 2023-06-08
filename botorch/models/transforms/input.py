14bf26a533c7912f9a3f2c2100a4214c2c1e6187
Max Balandat <balandat@meta.com>
2023-06-08 08:23:22 -0700

    Make AffineInputTransform take both lists and tensors as indices arg

--- botorch/models/transforms/input.py
+++ botorch/models/transforms/input.py
@@ -324,7 +324,7 @@ class AffineInputTransform(ReversibleInputTransform, Module):
         d: int,
         coefficient: Tensor,
         offset: Tensor,
-        indices: Optional[List[int]] = None,
+        indices: Optional[Union[List[int], Tensor]] = None,
         batch_shape: torch.Size = torch.Size(),  # noqa: B008
         transform_on_train: bool = True,
         transform_on_eval: bool = True,
@@ -342,7 +342,8 @@ class AffineInputTransform(ReversibleInputTransform, Module):
             offset: Tensor of offset coefficients, shape must to be
                 broadcastable with `(batch_shape x n x d)`-dim input tensors.
             indices: The indices of the inputs to transform. If omitted,
-                take all dimensions of the inputs into account.
+                take all dimensions of the inputs into account. Either a list of ints
+                or a Tensor of type `torch.long`.
             batch_shape: The batch shape of the inputs (assuming input tensors
                 of shape `batch_shape x n x d`). If provided, perform individual
                 transformation per batch, otherwise uses a single transformation.
@@ -359,7 +360,7 @@ class AffineInputTransform(ReversibleInputTransform, Module):
         if (indices is not None) and (len(indices) == 0):
             raise ValueError("`indices` list is empty!")
         if (indices is not None) and (len(indices) > 0):
-            indices = torch.tensor(indices, dtype=torch.long)
+            indices = torch.as_tensor(indices, dtype=torch.long, device=coefficient.device)
             if len(indices) > d:
                 raise ValueError("Can provide at most `d` indices!")
             if (indices > d - 1).any():
@@ -603,7 +604,7 @@ class Normalize(AffineInputTransform):
         r"""Get the arguments necessary to construct an exact copy of the transform."""
         return {
             "d": self._d,
-            "indices": list(self.indices) if hasattr(self, "indices") else None,
+            "indices": getattr(self, "indices", None),
             "bounds": self.bounds,
             "batch_shape": self.batch_shape,
             "transform_on_train": self.transform_on_train,
