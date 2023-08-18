946f0b47d87e88206f0aa6165f7e3a55dea3e302
David Eriksson <deriksson@meta.com>
2023-08-18 16:25:40 -0700

    Dont use a Sphinx version later than 7.1.2 (#1990)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/1990
    
    Sphinx 7.2.0 or later leads to doc build failures.
    
    Reviewed By: esantorella
    
    Differential Revision: D48487417
    
    fbshipit-source-id: d2ceafa62e94f6a19f82529e868bd342cb691eb9

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ if os.environ.get("ALLOW_LATEST_GPYTORCH_LINOP"):
 # Read in pinned versions of the formatting tools
 FMT_REQUIRES += read_deps_from_file("requirements-fmt.txt")
 # Dev is test + formatting + docs generation
-DEV_REQUIRES = TEST_REQUIRES + FMT_REQUIRES + ["sphinx"]
+DEV_REQUIRES = TEST_REQUIRES + FMT_REQUIRES + ["sphinx<=7.1.2"]
 
 # read in README.md as the long description
 with open(os.path.join(root_dir, "README.md"), "r") as fh:
