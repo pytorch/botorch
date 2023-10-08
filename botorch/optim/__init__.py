ca9457b1ca894bf314db74d35cdca7f76274ecdd
Max Balandat <balandat@fb.com>
2023-10-08 07:59:01 -0700

    Mixed alternating optimizer (#1987)
    Summary:
    
    Adds an optimizer for settings with mixed (continuous and discrete) variables, that interleaves gradient-based optimization (for fixed discrete variables) with nearest-neighbor search over discrete variables (for fixed continuous variables). This will need more cleanup and testing.
    
    Differential Revision: D48419691

--- botorch/optim/__init__.py
+++ botorch/optim/__init__.py
@@ -32,6 +32,7 @@ from botorch.optim.optimize import (
     optimize_acqf_discrete_local_search,
     optimize_acqf_mixed,
 )
+from botorch.optim.optimize_alternating import optimize_acqf_mixed_alternating
 from botorch.optim.optimize_homotopy import optimize_acqf_homotopy
 from botorch.optim.stopping import ExpMAStoppingCriterion
 
@@ -50,6 +51,7 @@ __all__ = [
     "optimize_acqf_discrete",
     "optimize_acqf_discrete_local_search",
     "optimize_acqf_mixed",
+    "optimize_acqf_mixed_alternating",
     "optimize_acqf_homotopy",
     "module_to_array",
     "scipy_minimize",
