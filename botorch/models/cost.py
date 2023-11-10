d0d3ad8ff47f23c39976ab39e198f7de4d29ce67
Sam Daulton <sdaulton@meta.com>
2023-11-10 05:18:21 -0800

    Add MultiOutputFixedCostModel (#2093)
    Summary:
    Pull Request resolved: https://github.com/pytorch/botorch/pull/2093
    
    This is necessary for decoupled scenarios where the objectives have different costs
    
    Reviewed By: Balandat
    
    Differential Revision: D51116013
    
    fbshipit-source-id: da7fe4e7d528cd410b5fe0cd07250cc7c4f61551

--- botorch/models/cost.py
+++ botorch/models/cost.py
@@ -86,3 +86,40 @@ class AffineFidelityCostModel(DeterministicModel):
             "...f,f", X[..., self.fidelity_dims], self.weights.to(X)
         )
         return self.fixed_cost + lin_cost.unsqueeze(-1)
+
+
+class FixedCostModel(DeterministicModel):
+    r"""Deterministic, fixed cost model.
+
+    For each (q-batch) element of a candidate set `X`, this module computes a
+    fixed cost per objective.
+    """
+
+    def __init__(
+        self,
+        fixed_cost: Tensor,
+    ) -> None:
+        r"""
+        Args:
+            fixed_cost: A `m`-dim tensor containing the fixed cost of evaluating each
+                objective.
+        """
+        super().__init__()
+        self.register_buffer("fixed_cost", fixed_cost)
+        self._num_outputs = fixed_cost.shape[-1]
+
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate the cost on a candidate set X.
+
+        Computes the fixed cost of evaluating each objective for each element
+        of the q-batch.
+
+        Args:
+            X: A `batch_shape x q x d'`-dim tensor of candidate points.
+
+        Returns:
+            A `batch_shape x q x m`-dim tensor of costs.
+        """
+        view_shape = [1] * (X.ndim - 1) + [self._num_outputs]
+        expand_shape = X.shape[:-1] + torch.Size([self._num_outputs])
+        return self.fixed_cost.view(view_shape).expand(expand_shape)
