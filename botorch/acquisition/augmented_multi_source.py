32caa447fdec2327e2c279265c8f50976ab893b0
Andrea Ponti <andrea.ponti@unimib.it>
2023-12-18 11:11:21 +0100

    Add Augmented UCB acquisition function for multi-source

---
+++ botorch/acquisition/augmented_multi_source.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from typing import Dict, Optional, Tuple, Union
+
+import torch
+
+from botorch.acquisition import UpperConfidenceBound
+from botorch.acquisition.objective import PosteriorTransform
+from botorch.exceptions import UnsupportedError
+from botorch.models.model import Model
+from botorch.utils.transforms import t_batch_mode_transform
+from gpytorch.models import ExactGP
+from torch import Tensor
+
+
+class AugmentedUpperConfidenceBound(UpperConfidenceBound):
+    r"""Single-outcome Multi-Source Upper Confidence Bound (UCB).
+
+    Description...
+
+    `AUCB(x, s, y^+) =
+        ((mu(x) + sqrt(beta) * sigma(x)) - y^+) /
+        (c(s) (1 + abs(mu(x) - mu_s(x))))`,
+     where `mu` and `sigma` are the posterior mean and standard deviation of the AGP,
+     `mu_s` is the posterior mean of the GP modelling the s-th source and
+     c(s) is the cost of the source s.
+
+    Example:
+        >>> model = AugmentedSingleTaskGP(train_X, train_Y)
+        >>> UCB = AugmentedUpperConfidenceBound(model, beta=0.2)
+        >>> ucb = UCB(test_X)
+    """
+
+    def __init__(
+        self,
+        model: Model,
+        cost: Dict,
+        best_f: Union[float, Tensor],
+        beta: Union[float, Tensor],
+        posterior_transform: Optional[PosteriorTransform] = None,
+        maximize: bool = True,
+    ) -> None:
+        r"""Single-outcome Augmented Upper Confidence Bound.
+
+        Args:
+            model: A fitted single-outcome Augmented GP model.
+            beta: Either a scalar or a one-dim tensor with `b` elements (batch mode)
+                representing the trade-off parameter between mean and covariance
+            posterior_transform: A PosteriorTransform. If using a multi-output model,
+                a PosteriorTransform that transforms the multi-output posterior into a
+                single-output posterior is required.
+            maximize: If True, consider the problem a maximization problem.
+        """
+        if not hasattr(model, "models"):
+            raise UnsupportedError("Model have to be multi-source.")
+        super().__init__(
+            model=model,
+            beta=beta,
+            maximize=maximize,
+            posterior_transform=posterior_transform,
+        )
+        self.cost = cost
+        self.best_f = best_f
+
+    @t_batch_mode_transform(expected_q=1)
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate the Upper Confidence Bound on the candidate set X.
+
+        Args:
+            X: A `(b1 x ... bk) x 1 x d`-dim batched tensor of `d`-dim design points.
+
+        Returns:
+            A `(b1 x ... bk)`-dim tensor of Augmented Upper Confidence Bound values at
+            the given design points `X`.
+        """
+        alpha = torch.zeros(X.shape[0], dtype=X.dtype)
+        agp_mean, agp_sigma = self._mean_and_sigma(X[..., :-1])
+        cb = (self.best_f if self.maximize else -self.best_f) + (
+            (agp_mean if self.maximize else -agp_mean) + self.beta.sqrt() * agp_sigma
+        )
+        source_idxs = {
+            int(s.tolist()): torch.where(torch.round(X[..., -1], decimals=0) == s)[0]
+            for s in torch.round(X[..., -1], decimals=0).unique()
+        }
+        for s in source_idxs:
+            mean, sigma = self._mean_and_sigma(
+                X[source_idxs[s], :, :-1], self.model.models[s]
+            )
+            alpha[source_idxs[s]] = (
+                cb[source_idxs[s]]
+                / self.cost[s]
+                * (1 + torch.abs(agp_mean[source_idxs[s]] - mean))
+            )
+        return alpha
+
+    def _mean_and_sigma(
+        self,
+        X: Tensor,
+        model: ExactGP = None,
+        compute_sigma: bool = True,
+        min_var: float = 1e-12,
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        r"""Computes the first and second moments of the model posterior.
+
+        Args:
+            X: `batch_shape x q x d`-dim Tensor of model inputs.
+            model: the model to use. If None, self is used.
+            compute_sigma: Boolean indicating whether to compute the second
+                moment (default: True).
+            min_var: The minimum value the variance is clamped too. Should be positive.
+
+        Returns:
+            A tuple of tensors containing the first and second moments of the model
+            posterior. Removes the last two dimensions if they have size one. Only
+            returns a single tensor of means if compute_sigma is True.
+        """
+        self.to(device=X.device)
+        if model is None:
+            posterior = self.model.posterior(
+                X=X, posterior_transform=self.posterior_transform
+            )
+        else:
+            posterior = model.posterior(
+                X=X, posterior_transform=self.posterior_transform
+            )
+        mean = posterior.mean.squeeze(-2).squeeze(-1)  # removing redundant dimensions
+        if not compute_sigma:
+            return mean, None
+        sigma = posterior.variance.clamp_min(min_var).sqrt().view(mean.shape)
+        return mean, sigma
