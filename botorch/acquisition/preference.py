58c183e7d001d92e0f2c92707fdce991a1515edf
RaulAstudillo06 <astudillo0606@gmail.com>
2024-02-06 21:52:06 -0800

    Comply with flake8

--- botorch/acquisition/preference.py
+++ botorch/acquisition/preference.py
@@ -166,9 +166,9 @@ class qExpectedUtilityOfBestOption(MCAcquisitionFunction):
             objective: The MCAcquisitionObjective under which the samples are evaluated.
                 Defaults to `IdentityMCObjective()`.
             posterior_transform: A PosteriorTransform (optional).
-            X_pending:  A `m x d`-dim Tensor of `m` design points that have been submitted
-                for function evaluation but have not yet been evaluated. Concatenated
-                into X upon forward call. Copied and set to have no gradient.
+            X_pending:  A `m x d`-dim Tensor of `m` design points that have been
+            submitted for function evaluation but have not yet been evaluated.
+            Concatenated into X upon forward call. Copied and set to have no gradient.
         """
         super().__init__(
             model=pref_model,
