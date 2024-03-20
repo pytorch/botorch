d1d08a7df41c0182dd2ccf130b4750083aa071f4
Max Balandat <balandat@meta.com>
2024-03-20 13:48:15 -0700

    Fix version sorting for website (#2257)
    Summary:
    Addresses https://github.com/pytorch/botorch/issues/2256
    
    Pull Request resolved: https://github.com/pytorch/botorch/pull/2257
    
    Reviewed By: SebastianAment
    
    Differential Revision: D55113779
    
    Pulled By: Balandat
    
    fbshipit-source-id: 98c16507dfffb03d5823a63f2d419d798616866f

--- scripts/versions.js
+++ scripts/versions.js
@@ -13,10 +13,21 @@ const CompLibrary = require('../../core/CompLibrary');
 
 const Container = CompLibrary.Container;
 
+// We need to sort the versions descending numerically (not based on string representations)
+function compareVersions(v1, v2) {
+  const v1_arr = v1.split(".");
+  const v2_arr = v2.split(".");
+  for (let i = 0; i < v1_arr.length; i++) {
+    const diff = parseInt(v2_arr[i]) - parseInt(v1_arr[i]);
+    if (diff != 0) {return diff}
+  }
+  return 0
+}
+
 const CWD = process.cwd();
 
 const versions = require(`${CWD}/_versions.json`);
-versions.sort().reverse();
+versions.sort(compareVersions);
 
 function Versions(props) {
   const {config: siteConfig} = props;
