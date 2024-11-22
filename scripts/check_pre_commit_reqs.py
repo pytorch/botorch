228a02cd7ea57a889d2945535d0324caf4151848
Rhys Goodall <rhys.goodall@outlook.com>
2024-11-22 10:08:28 -0500

    fix: syntax error

--- scripts/check_pre_commit_reqs.py
+++ scripts/check_pre_commit_reqs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-+# Copyright (c) Meta Platforms, Inc. and affiliates.
-+#
-+# This source code is licensed under the MIT license found in the
-+# LICENSE file in the root directory of this source tree.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 import sys
 from pathlib import Path
