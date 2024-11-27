bb22e7f9c12075f491b97226306595fd71a327bd
Cristian Lara <cristianlara@meta.com>
2024-11-27 14:00:36 -0600

    [Tutorials] Add to docusaurus sidebar
    Needed to convert the json file to js to create the sidebar by parsing the tutorials.json file.

---
+++ website/sidebars.js
@@ -0,0 +1,44 @@
+const tutorials = () => {
+  const allTutorialMetadata = require('./tutorials.json');
+  const tutorialsSidebar = [{
+    type: 'category',
+    label: 'Tutorials',
+    collapsed: false,
+    items: [
+      {
+        type: 'doc',
+        id: 'tutorials/index',
+        label: 'Overview',
+      },
+    ],
+  },];
+  for (var category in allTutorialMetadata) {
+    const categoryItems = allTutorialMetadata[category];
+    const items = [];
+    categoryItems.map(item => {
+      items.push({
+        type: 'doc',
+        label: item.title,
+        id: `tutorials/${item.id}/index`,
+      });
+    });
+
+    tutorialsSidebar.push({
+      type: 'category',
+      label: category,
+      items: items,
+    });
+  }
+  return tutorialsSidebar;
+};
+
+export default {
+  "docs": {
+    "About": ["introduction", "design_philosophy", "botorch_and_ax", "papers"],
+    "General": ["getting_started"],
+    "Basic Concepts": ["overview", "models", "posteriors", "acquisition", "optimization"],
+    "Advanced Topics": ["constraints", "objectives", "batching", "samplers"],
+    "Multi-Objective Optimization": ["multi_objective"]
+  },
+  tutorials: tutorials(),
+}
