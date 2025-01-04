12e3c4aaff28c3912f38b07966a60c91ff0007d6
Max Balandat <balandat@meta.com>
2025-01-03 16:16:05 -0800

    Fix line length

--- botorch/optim/core.py
+++ botorch/optim/core.py
@@ -35,7 +35,8 @@ except ImportError:  # pragma: no cover
 
 _LBFGSB_MAXITER_MAXFUN_REGEX = re.compile(  # regex for maxiter and maxfun messages
     # Note that the messages changed with scipy 1.15, hence the different matching here.
-    "STOP: TOTAL NO. (of|OF) (ITERATIONS REACHED LIMIT|(f AND g|F,G) EVALUATIONS EXCEEDS LIMIT)"
+    "STOP: TOTAL NO. (of|OF) "
+    + "(ITERATIONS REACHED LIMIT|(f AND g|F,G) EVALUATIONS EXCEEDS LIMIT)"
 )
 
 
