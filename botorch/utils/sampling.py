1cdd7777373da593528dc8cb15fa5ffcb3000bd5
Sait Cakmak <saitcakmak@outlook.com>
2025-02-24 18:25:48 -0500

    Update botorch/utils/sampling.py

--- botorch/utils/sampling.py
+++ botorch/utils/sampling.py
@@ -1091,7 +1091,6 @@ def boltzmann_sample(
             succesively decreased by 'temp_decrease'.
         replacement: If True, samples are drawn with replacement, allowing duplicates.
         temp_decrease: The rate at which temperature decreases in case of inf weights.
-        
     Returns:
         A [batch_shape] x num_samples tensor of indices of sampled positions.
     """
