505d89c85d1655a57b36744986fc9b1743526a39
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2025-03-19 11:07:44 +0100

    probabilities_of_feasibility

--- botorch/acquisition/monte_carlo.py
+++ botorch/acquisition/monte_carlo.py
@@ -47,7 +47,10 @@ from botorch.exceptions.errors import UnsupportedError
 from botorch.exceptions.warnings import legacy_ei_numerics_warning
 from botorch.models.model import Model
 from botorch.sampling.base import MCSampler
-from botorch.utils.objective import compute_smoothed_feasibility_indicator
+from botorch.utils.objective import (
+    compute_probabilities_of_feasibility_indicator,
+    compute_smoothed_feasibility_indicator,
+)
 from botorch.utils.transforms import (
     concatenate_pending_points,
     match_batch_shape,
@@ -188,6 +191,7 @@ class SampleReducingMCAcquisitionFunction(MCAcquisitionFunction):
         sample_reduction: SampleReductionProtocol = torch.mean,
         q_reduction: SampleReductionProtocol = torch.amax,
         constraints: list[Callable[[Tensor], Tensor]] | None = None,
+        probabilities_of_feasibility: list[Callable[[Tensor], Tensor]] | None = None,
         eta: Tensor | float = 1e-3,
         fat: bool = False,
     ):
@@ -248,6 +252,7 @@ class SampleReducingMCAcquisitionFunction(MCAcquisitionFunction):
         self._sample_reduction = partial(sample_reduction, dim=sample_dim)
         self._q_reduction = partial(q_reduction, dim=-1)
         self._constraints = constraints
+        self._probabilities_of_feasibility = probabilities_of_feasibility
         self._eta = eta
         self._fat = fat
 
@@ -326,6 +331,18 @@ class SampleReducingMCAcquisitionFunction(MCAcquisitionFunction):
                 eta=self._eta,
                 log=self._log,
                 fat=self._fat,
+            )
+            acqval = acqval.add(ind) if self._log else acqval.mul(ind)
+        if self._probabilities_of_feasibility is not None:
+            if not self._log and (acqval < 0).any():
+                raise ValueError(
+                    "Constraint-weighting requires unconstrained "
+                    "acquisition values to be non-negative."
+                )
+            ind = compute_probabilities_of_feasibility_indicator(
+                probabilities_of_feasibility=self._probabilities_of_feasibility,
+                samples=samples,
+                log=self._log,
             )
             acqval = acqval.add(ind) if self._log else acqval.mul(ind)
         return acqval
