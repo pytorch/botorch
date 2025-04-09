dd30bf90f1bdfb920ca3386fa5af39e03195adff
pbrunzema <paul.brunzema@dsme.rwth-aachen.de>
2025-04-09 09:09:46 +0200

    add test for bll posterior sampling

--- botorch_community/acquisition/bll_thompson_sampling.py
+++ botorch_community/acquisition/bll_thompson_sampling.py
@@ -17,6 +17,9 @@ from torch.func import grad
 
 
 class BLLMaxPosteriorSampling:
+    """TODO: Integrate into `SamplingStrategy` (note, here we dont require
+    X_cand to be passed but optimize the sample paths numerically as a default)."""
+
     def __init__(
         self,
         model: AbstractBLLModel,
@@ -35,7 +38,7 @@ class BLLMaxPosteriorSampling:
                 instance of `AbstractBLLModel`.
             num_restarts: Number of restarts for optimization-based sampling.
                 Defaults to 10.
-            bounds: Tensor of shape (2, num_inputs) specifying the lower and upper
+            bounds: Tensor of shape `2 x d` specifying the lower and upper
                 bounds for sampling. If None, defaults to [(0, 1)] for each input
                 dimension.
             discrete_inputs: If True, assumes the input space is discrete and will be
@@ -77,6 +80,18 @@ class BLLMaxPosteriorSampling:
     def __call__(
         self, X_cand: torch.Tensor = None, num_samples: int = 1
     ) -> torch.Tensor:
+        r"""Sample from a Bayesian last layer model posterior.
+
+        Args:
+            X_cand: A `N x d`-dim Tensor from which to sample (in the `N`
+                dimension) according to the maximum posterior value under the objective.
+                NOTE: X_cand is only accepted if `discrete_inputs` is `True`!
+            num_samples: The number of samples to draw.
+
+        Returns:
+            A `num_samples x d`-dim Tensor of maximum posterior values from the model,
+            where `X[i, :]` is the `i`-th sample.
+        """
         if self.discrete_inputs and X_cand is None:
             raise ValueError("X_cand must be provided if `discrete_inputs` is True.")
 
@@ -134,7 +149,9 @@ def _optimize_sample_path(
         device: Device on which to store the candidate points.
 
     Returns:
-        Candidate points and corresponding function values.
+        Candidate points and corresponding function values as a tuple with a
+        `num_restarts x d`-dim Tensor and `num_restarts x num_outputs`-dim Tensor,
+        respectively.
     """
     X_cand = torch.empty(num_restarts, f.num_inputs, dtype=torch.float64, device=device)
     Y_cand = torch.empty(
@@ -154,6 +171,10 @@ def _optimize_sample_path(
     # generate random initial conditions
     x0s = np.random.rand(num_restarts, f.num_inputs)
 
+    lb = lb.numpy()
+    ub = ub.numpy()
+
+    optimization_successful = False
     for j in range(num_restarts):
         # map to bounds
         x0 = lb + (ub - lb) * x0s[j]
@@ -163,11 +184,17 @@ def _optimize_sample_path(
             func, x0, jac=grad_func, bounds=bounds, method="L-BFGS-B"
         )
 
+        # check if optimization was successful
+        if res.success:
+            optimization_successful = True
         if not res.success:
             logger.warning(f"Optimization failed with message: {res.message}")
 
         # store the candidate
         X_cand[j, :] = torch.from_numpy(res.x).to(dtype=torch.float64)
         Y_cand[j] = torch.tensor([-res.fun], dtype=torch.float64)
+
+    if not optimization_successful:
+        raise RuntimeError("All optimization attempts on the sample path failed.")
 
     return X_cand, Y_cand
