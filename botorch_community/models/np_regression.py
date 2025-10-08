e390e135c5327f832275d69ac485d7844e68e234
Max Balandat <balandat@meta.com>
2025-10-08 06:28:47 -0700

    Add missing license header to np_regression.py module (#3043)
    Reviewed By: saitcakmak
    
    Differential Revision: D84151472
    
    Pulled By: Balandat
    
    fbshipit-source-id: 0e97db6a280fd2703e188b30c769aef46a3526b4

--- botorch_community/models/np_regression.py
+++ botorch_community/models/np_regression.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 r"""
 Neural Process Regression models based on PyTorch models.
 
